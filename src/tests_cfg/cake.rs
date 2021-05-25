bbe80fa87398bf5814aa40262086f986dd9a4686
Billy Chan <ccw.billy.123@gmail.com>
2021-05-25 10:42:49 +0800

    ActiveModel derive macro

--- src/tests_cfg/cake.rs
+++ src/tests_cfg/cake.rs
@@ -11,13 +11,6 @@ pub struct Model {
     pub name: String,
 }
 
-// can we generate this?
-#[derive(Clone, Debug)]
-pub struct ActiveModel {
-    pub id: Action<i32>,
-    pub name: Action<String>,
-}
-
 #[derive(Copy, Clone, Debug, EnumIter, DeriveColumn)]
 pub enum Column {
     Id,
@@ -79,48 +72,5 @@ impl Model {
 
     pub fn find_filling(&self) -> Select<super::filling::Entity> {
         Entity::find_related().belongs_to::<Entity>(self)
-    }
-}
-
-// can we generate this?
-impl ActiveModelOf<Model> for ActiveModel {
-    fn from_model(m: Model) -> Self {
-        Self::from(m)
-    }
-}
-
-// can we generate this?
-impl From<Model> for ActiveModel {
-    fn from(m: Model) -> Self {
-        Self {
-            id: Action::Set(m.id),
-            name: Action::Set(m.name),
-        }
-    }
-}
-
-// can we generate this?
-impl ActiveModelTrait for ActiveModel {
-    type Column = Column;
-
-    fn get(&self, c: Self::Column) -> Action<Value> {
-        match c {
-            Column::Id => self.id.clone().into_action_value(),
-            Column::Name => self.name.clone().into_action_value(),
-        }
-    }
-
-    fn set(&mut self, c: Self::Column, v: Value) {
-        match c {
-            Column::Id => self.id = Action::Set(v.unwrap()),
-            Column::Name => self.name = Action::Set(v.unwrap()),
-        }
-    }
-
-    fn unset(&mut self, c: Self::Column) {
-        match c {
-            Column::Id => self.id = Action::Unset,
-            Column::Name => self.name = Action::Unset,
-        }
     }
 }
