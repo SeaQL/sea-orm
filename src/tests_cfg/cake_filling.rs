7bf79ae5bffb19be506e3a3ac15e7659fa3389af
Billy Chan <ccw.billy.123@gmail.com>
2021-06-30 11:02:04 +0800

    Re-export strum_macros

--- src/tests_cfg/cake_filling.rs
+++ src/tests_cfg/cake_filling.rs
@@ -17,12 +17,14 @@ pub struct Model {
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveColumn)]
+#[strum(crate_path = "sea_orm::strum")]
 pub enum Column {
     CakeId,
     FillingId,
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DerivePrimaryKey)]
+#[strum(crate_path = "sea_orm::strum")]
 pub enum PrimaryKey {
     CakeId,
     FillingId,
@@ -35,6 +37,7 @@ impl PrimaryKeyTrait for PrimaryKey {
 }
 
 #[derive(Copy, Clone, Debug, EnumIter)]
+#[strum(crate_path = "sea_orm::strum")]
 pub enum Relation {
     Cake,
     Filling,
