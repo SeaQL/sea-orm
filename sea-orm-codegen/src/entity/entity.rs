a94224e0f29a0cb7693f0a10f9337e96afd4936e
Billy Chan <ccw.billy.123@gmail.com>
2021-07-03 00:02:45 +0800

    Unit test sea-orm-codegen

--- sea-orm-codegen/src/entity/entity.rs
+++ sea-orm-codegen/src/entity/entity.rs
@@ -111,15 +111,242 @@ impl Entity {
             .collect()
     }
 
-    pub fn get_relation_rel_find_helpers(&self) -> Vec<Ident> {
-        self.relations
-            .iter()
-            .map(|rel| rel.get_rel_find_helper())
-            .collect()
-    }
-
     pub fn get_primary_key_auto_increment(&self) -> Ident {
         let auto_increment = self.columns.iter().any(|col| col.auto_increment);
         format_ident!("{}", auto_increment)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{Column, Entity, PrimaryKey, Relation, RelationType};
+    use quote::format_ident;
+    use sea_query::ColumnType;
+
+    fn setup() -> Entity {
+        Entity {
+            table_name: "special_cake".to_owned(),
+            columns: vec![
+                Column {
+                    name: "id".to_owned(),
+                    col_type: ColumnType::String(None),
+                    auto_increment: false,
+                    not_null: false,
+                    unique: false,
+                },
+                Column {
+                    name: "name".to_owned(),
+                    col_type: ColumnType::String(None),
+                    auto_increment: false,
+                    not_null: false,
+                    unique: false,
+                },
+            ],
+            relations: vec![
+                Relation {
+                    ref_table: "fruit".to_owned(),
+                    columns: vec!["id".to_owned()],
+                    ref_columns: vec!["cake_id".to_owned()],
+                    rel_type: RelationType::HasOne,
+                },
+                Relation {
+                    ref_table: "filling".to_owned(),
+                    columns: vec!["id".to_owned()],
+                    ref_columns: vec!["cake_id".to_owned()],
+                    rel_type: RelationType::HasOne,
+                },
+            ],
+            primary_keys: vec![PrimaryKey {
+                name: "id".to_owned(),
+            }],
+        }
+    }
+
+    #[test]
+    fn test_get_table_name_snake_case() {
+        let entity = setup();
+
+        assert_eq!(
+            entity.get_table_name_snake_case(),
+            "special_cake".to_owned()
+        );
+    }
+
+    #[test]
+    fn test_get_table_name_camel_case() {
+        let entity = setup();
+
+        assert_eq!(entity.get_table_name_camel_case(), "SpecialCake".to_owned());
+    }
+
+    #[test]
+    fn test_get_table_name_snake_case_ident() {
+        let entity = setup();
+
+        assert_eq!(
+            entity.get_table_name_snake_case_ident(),
+            format_ident!("{}", "special_cake")
+        );
+    }
+
+    #[test]
+    fn test_get_table_name_camel_case_ident() {
+        let entity = setup();
+
+        assert_eq!(
+            entity.get_table_name_camel_case_ident(),
+            format_ident!("{}", "SpecialCake")
+        );
+    }
+
+    #[test]
+    fn test_get_column_names_snake_case() {
+        let entity = setup();
+
+        for (i, elem) in entity.get_column_names_snake_case().into_iter().enumerate() {
+            assert_eq!(elem, entity.columns[i].get_name_snake_case());
+        }
+    }
+
+    #[test]
+    fn test_get_column_names_camel_case() {
+        let entity = setup();
+
+        for (i, elem) in entity.get_column_names_camel_case().into_iter().enumerate() {
+            assert_eq!(elem, entity.columns[i].get_name_camel_case());
+        }
+    }
+
+    #[test]
+    fn test_get_column_rs_types() {
+        let entity = setup();
+
+        for (i, elem) in entity.get_column_rs_types().into_iter().enumerate() {
+            assert_eq!(
+                elem.to_string(),
+                entity.columns[i].get_rs_type().to_string()
+            );
+        }
+    }
+
+    #[test]
+    fn test_get_column_defs() {
+        let entity = setup();
+
+        for (i, elem) in entity.get_column_defs().into_iter().enumerate() {
+            assert_eq!(elem.to_string(), entity.columns[i].get_def().to_string());
+        }
+    }
+
+    #[test]
+    fn test_get_primary_key_names_snake_case() {
+        let entity = setup();
+
+        for (i, elem) in entity
+            .get_primary_key_names_snake_case()
+            .into_iter()
+            .enumerate()
+        {
+            assert_eq!(elem, entity.primary_keys[i].get_name_snake_case());
+        }
+    }
+
+    #[test]
+    fn test_get_primary_key_names_camel_case() {
+        let entity = setup();
+
+        for (i, elem) in entity
+            .get_primary_key_names_camel_case()
+            .into_iter()
+            .enumerate()
+        {
+            assert_eq!(elem, entity.primary_keys[i].get_name_camel_case());
+        }
+    }
+
+    #[test]
+    fn test_get_relation_ref_tables_snake_case() {
+        let entity = setup();
+
+        for (i, elem) in entity
+            .get_relation_ref_tables_snake_case()
+            .into_iter()
+            .enumerate()
+        {
+            assert_eq!(elem, entity.relations[i].get_ref_table_snake_case());
+        }
+    }
+
+    #[test]
+    fn test_get_relation_ref_tables_camel_case() {
+        let entity = setup();
+
+        for (i, elem) in entity
+            .get_relation_ref_tables_camel_case()
+            .into_iter()
+            .enumerate()
+        {
+            assert_eq!(elem, entity.relations[i].get_ref_table_camel_case());
+        }
+    }
+
+    #[test]
+    fn test_get_relation_defs() {
+        let entity = setup();
+
+        for (i, elem) in entity.get_relation_defs().into_iter().enumerate() {
+            assert_eq!(elem.to_string(), entity.relations[i].get_def().to_string());
+        }
+    }
+
+    #[test]
+    fn test_get_relation_rel_types() {
+        let entity = setup();
+
+        for (i, elem) in entity.get_relation_rel_types().into_iter().enumerate() {
+            assert_eq!(elem, entity.relations[i].get_rel_type());
+        }
+    }
+
+    #[test]
+    fn test_get_relation_columns_camel_case() {
+        let entity = setup();
+
+        for (i, elem) in entity
+            .get_relation_columns_camel_case()
+            .into_iter()
+            .enumerate()
+        {
+            assert_eq!(elem, entity.relations[i].get_column_camel_case());
+        }
+    }
+
+    #[test]
+    fn test_get_relation_ref_columns_camel_case() {
+        let entity = setup();
+
+        for (i, elem) in entity
+            .get_relation_ref_columns_camel_case()
+            .into_iter()
+            .enumerate()
+        {
+            assert_eq!(elem, entity.relations[i].get_ref_column_camel_case());
+        }
+    }
+
+    #[test]
+    fn test_get_primary_key_auto_increment() {
+        let mut entity = setup();
+
+        assert_eq!(
+            entity.get_primary_key_auto_increment(),
+            format_ident!("{}", false)
+        );
+
+        entity.columns[0].auto_increment = true;
+        assert_eq!(
+            entity.get_primary_key_auto_increment(),
+            format_ident!("{}", true)
+        );
     }
 }
---
+++ sea-orm-codegen/tests/mod.rs
@@ -0,0 +1,127 @@
+mod entity;
+
+use entity::*;
+
+use sea_orm::{entity::*, error::*, MockDatabase, MockExecResult, Transaction};
+
+#[async_std::test]
+async fn test_insert() -> Result<(), DbErr> {
+    let exec_result = MockExecResult {
+        last_insert_id: 1,
+        rows_affected: 1,
+    };
+
+    let db = MockDatabase::new()
+        .append_exec_results(vec![exec_result.clone()])
+        .into_connection();
+
+    let apple = cake::ActiveModel {
+        name: Set("Apple Pie".to_owned()),
+        ..Default::default()
+    };
+
+    let insert_result = cake::Entity::insert(apple).exec(&db).await?;
+
+    assert_eq!(insert_result.last_insert_id, exec_result.last_insert_id);
+
+    assert_eq!(
+        db.into_transaction_log(),
+        vec![Transaction::from_sql_and_values(
+            r#"INSERT INTO "cake" ("name") VALUES ($1)"#,
+            vec!["Apple Pie".into()]
+        )]
+    );
+
+    Ok(())
+}
+
+#[async_std::test]
+async fn test_select() -> Result<(), DbErr> {
+    let query_results = vec![cake_filling::Model {
+        cake_id: 2,
+        filling_id: 3,
+    }];
+
+    let db = MockDatabase::new()
+        .append_query_results(vec![query_results.clone()])
+        .into_connection();
+
+    let selected_models = cake_filling::Entity::find_by_id((2, 3)).all(&db).await?;
+
+    assert_eq!(selected_models, query_results);
+
+    assert_eq!(
+        db.into_transaction_log(),
+        vec![Transaction::from_sql_and_values([
+                r#"SELECT "cake_filling"."cake_id", "cake_filling"."filling_id" FROM "cake_filling""#,
+                r#"WHERE "cake_filling"."cake_id" = $1 AND "cake_filling"."filling_id" = $2"#,
+            ].join(" ").as_str(),
+            vec![2i32.into(), 3i32.into()]
+        )]
+    );
+
+    Ok(())
+}
+
+#[async_std::test]
+async fn test_update() -> Result<(), DbErr> {
+    let exec_result = MockExecResult {
+        last_insert_id: 1,
+        rows_affected: 1,
+    };
+
+    let db = MockDatabase::new()
+        .append_exec_results(vec![exec_result.clone()])
+        .into_connection();
+
+    let orange = fruit::ActiveModel {
+        id: Set(1),
+        name: Set("Orange".to_owned()),
+        ..Default::default()
+    };
+
+    let updated_model = fruit::Entity::update(orange.clone()).exec(&db).await?;
+
+    assert_eq!(updated_model, orange);
+
+    assert_eq!(
+        db.into_transaction_log(),
+        vec![Transaction::from_sql_and_values(
+            r#"UPDATE "fruit" SET "name" = $1 WHERE "fruit"."id" = $2"#,
+            vec!["Orange".into(), 1i32.into()]
+        )]
+    );
+
+    Ok(())
+}
+
+#[async_std::test]
+async fn test_delete() -> Result<(), DbErr> {
+    let exec_result = MockExecResult {
+        last_insert_id: 1,
+        rows_affected: 1,
+    };
+
+    let db = MockDatabase::new()
+        .append_exec_results(vec![exec_result.clone()])
+        .into_connection();
+
+    let orange = fruit::ActiveModel {
+        id: Set(3),
+        ..Default::default()
+    };
+
+    let delete_result = fruit::Entity::delete(orange).exec(&db).await?;
+
+    assert_eq!(delete_result.rows_affected, exec_result.rows_affected);
+
+    assert_eq!(
+        db.into_transaction_log(),
+        vec![Transaction::from_sql_and_values(
+            r#"DELETE FROM "fruit" WHERE "fruit"."id" = $1"#,
+            vec![3i32.into()]
+        )]
+    );
+
+    Ok(())
+}
