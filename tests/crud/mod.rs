f60a1c32a31ba56916457989e15160c0268efa96
Sam Samai <sam@studio2pi.com.au>
2021-07-03 21:18:35 +1000

    Test bakery create

--- tests/crud/mod.rs
+++ tests/crud/mod.rs
@@ -8,7 +8,17 @@ pub async fn create_bakery(db: &DbConn) -> Result<(), ExecErr> {
         profit_margin: Set(10.4),
         ..Default::default()
     };
-    let _res: InsertResult = Bakery::insert(seaside_bakery).exec(db).await?;
+    let res: InsertResult = Bakery::insert(seaside_bakery).exec(db).await?;
+
+    let bakery: Option<bakery::Model> = Bakery::find_by_id(res.last_insert_id)
+        .one(db)
+        .await
+        .map_err(|_| ExecErr)?;
+
+    assert!(bakery.is_some());
+    let bakery_model = bakery.unwrap();
+    assert_eq!(bakery_model.name, "SeaSide Bakery");
+    assert_eq!(bakery_model.profit_margin, 10.4);
 
     Ok(())
 }
