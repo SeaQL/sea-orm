c73e10cc5918a57d7ace811843271a0087a7190c
Billy Chan <ccw.billy.123@gmail.com>
2021-07-09 12:52:28 +0800

    Fix clippy warming

--- src/lib.rs
+++ src/lib.rs
@@ -177,9 +177,9 @@
 //! Licensed under either of
 //! 
 //! -   Apache License, Version 2.0
-//!     ([LICENSE-APACHE](LICENSE-APACHE) or http://www.apache.org/licenses/LICENSE-2.0)
+//!     ([LICENSE-APACHE](LICENSE-APACHE) or <http://www.apache.org/licenses/LICENSE-2.0>)
 //! -   MIT license
-//!     ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
+//!     ([LICENSE-MIT](LICENSE-MIT) or <http://opensource.org/licenses/MIT>)
 //! 
 //! at your option.
 //! 
