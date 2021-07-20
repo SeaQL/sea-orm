c4cd9997e7bce3e164df2c9ced7fb66c396ad639
Billy Chan <ccw.billy.123@gmail.com>
2021-07-20 11:47:00 +0800

    Handle conjunction table

--- sea-orm-codegen/src/entity/mod.rs
+++ sea-orm-codegen/src/entity/mod.rs
@@ -1,5 +1,6 @@
 mod base_entity;
 mod column;
+mod conjunct_relation;
 mod generator;
 mod primary_key;
 mod relation;
@@ -8,6 +9,7 @@ mod writer;
 
 pub use base_entity::*;
 pub use column::*;
+pub use conjunct_relation::*;
 pub use generator::*;
 pub use primary_key::*;
 pub use relation::*;
