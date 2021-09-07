022bee06277e047f229825b664ab07eaf3d036ee
Chris Tsang <tyt2y7@gmail.com>
2021-09-07 20:37:32 +0800

    cargo fmt

--- sea-orm-macros/src/derives/mod.rs
+++ sea-orm-macros/src/derives/mod.rs
@@ -2,16 +2,16 @@ mod active_model;
 mod active_model_behavior;
 mod column;
 mod entity;
+mod entity_model;
 mod from_query_result;
 mod model;
 mod primary_key;
-mod entity_model;
 
 pub use active_model::*;
 pub use active_model_behavior::*;
 pub use column::*;
 pub use entity::*;
+pub use entity_model::*;
 pub use from_query_result::*;
 pub use model::*;
 pub use primary_key::*;
-pub use entity_model::*;
--- sea-orm-macros/src/lib.rs
+++ sea-orm-macros/src/lib.rs
@@ -1,7 +1,7 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
-use syn::{DeriveInput, parse_macro_input};
+use syn::{parse_macro_input, DeriveInput};
 
 mod derives;
 
