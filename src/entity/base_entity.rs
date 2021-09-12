71bfd508fdf7d58bc9b00be0126561e0a363dc6d
Marco Napetti <nappa85@therocktrading.com>
2021-09-12 10:44:23 +0200

    Fix postgres test

--- src/entity/base_entity.rs
+++ src/entity/base_entity.rs
@@ -344,7 +344,7 @@ pub trait EntityTrait: EntityName {
     /// assert_eq!(
     ///     db.into_transaction_log(),
     ///     vec![Transaction::from_sql_and_values(
-    ///         DbBackend::Postgres, r#"INSERT INTO "cake" ("name") VALUES ($1), ($2)"#,
+    ///         DbBackend::Postgres, r#"INSERT INTO "cake" ("name") VALUES ($1), ($2) RETURNING "id""#,
     ///         vec!["Apple Pie".into(), "Orange Scone".into()]
     ///     )]);
     /// ```
