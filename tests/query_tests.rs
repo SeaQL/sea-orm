301ee21b0055b582092501695b0ccb2f794da1ea
Marco Napetti <nappa85@therocktrading.com>
2021-09-15 11:52:26 +0200

    Transaction with references test

--- tests/query_tests.rs
+++ tests/query_tests.rs
@@ -1,6 +1,7 @@
 pub mod common;
 
 pub use common::{bakery_chain::*, setup::*, TestContext};
+use sea_orm::{DatabaseTransaction, DbErr};
 pub use sea_orm::entity::*;
 pub use sea_orm::{QueryFilter, ConnectionTrait};
 
@@ -222,8 +223,6 @@ pub async fn find_all_filter_with_results() {
     feature = "sqlx-postgres"
 ))]
 pub async fn transaction() {
-    use sea_orm::DbErr;
-
     let ctx = TestContext::new("find_all_filter_with_results").await;
 
     ctx.db.transaction::<_, (), DbErr>(|txn| Box::pin(async move {
@@ -232,16 +231,16 @@ pub async fn transaction() {
             profit_margin: Set(10.4),
             ..Default::default()
         }
-        .save(txn)
-        .await?;
+            .save(txn)
+            .await?;
 
         let _ = bakery::ActiveModel {
             name: Set("Top Bakery".to_owned()),
             profit_margin: Set(15.0),
             ..Default::default()
         }
-        .save(txn)
-        .await?;
+            .save(txn)
+            .await?;
 
         let bakeries = Bakery::find()
             .filter(bakery::Column::Name.contains("Bakery"))
@@ -254,4 +253,49 @@ pub async fn transaction() {
     })).await.unwrap();
 
     ctx.delete().await;
+}
+
+#[sea_orm_macros::test]
+#[cfg(any(
+    feature = "sqlx-mysql",
+    feature = "sqlx-sqlite",
+    feature = "sqlx-postgres"
+))]
+pub async fn transaction_with_reference() {
+    let ctx = TestContext::new("find_all_filter_with_results").await;
+    let name1 = "SeaSide Bakery";
+    let name2 = "Top Bakery";
+    let search_name = "Bakery";
+    ctx.db.transaction(|txn| _transaction_with_reference(txn, name1, name2, search_name)).await.unwrap();
+
+    ctx.delete().await;
+}
+
+fn _transaction_with_reference<'a>(txn: &'a DatabaseTransaction, name1: &'a str, name2: &'a str, search_name: &'a str) -> std::pin::Pin<Box<dyn std::future::Future<Output=Result<(), DbErr>> + Send + 'a>> {
+    Box::pin(async move {
+        let _ = bakery::ActiveModel {
+            name: Set(name1.to_owned()),
+            profit_margin: Set(10.4),
+            ..Default::default()
+        }
+            .save(txn)
+            .await?;
+
+        let _ = bakery::ActiveModel {
+            name: Set(name2.to_owned()),
+            profit_margin: Set(15.0),
+            ..Default::default()
+        }
+            .save(txn)
+            .await?;
+
+        let bakeries = Bakery::find()
+            .filter(bakery::Column::Name.contains(search_name))
+            .all(txn)
+            .await?;
+
+        assert_eq!(bakeries.len(), 2);
+
+        Ok(())
+    })
 }
