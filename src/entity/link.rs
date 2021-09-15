8a4996300fcf45e422db9e36d68930d09cd67962
Chris Tsang <tyt2y7@gmail.com>
2021-09-15 12:16:56 +0800

    Refactor Link

---
+++ src/entity/link.rs
@@ -0,0 +1,20 @@
+use crate::{EntityTrait, RelationDef, QuerySelect, Select};
+use sea_query::{JoinType};
+
+pub type LinkDef = RelationDef;
+
+pub trait Linked {
+    type FromEntity: EntityTrait;
+
+    type ToEntity: EntityTrait;
+
+    fn link(&self) -> Vec<LinkDef>;
+
+    fn find_linked(&self) -> Select<Self::ToEntity> {
+        let mut select = Select::new();
+        for rel in self.link().into_iter().rev() {
+            select = select.join_rev(JoinType::InnerJoin, rel);
+        }
+        select
+    }
+}
--- src/entity/relation.rs
+++ src/entity/relation.rs
@@ -30,22 +30,6 @@ where
     }
 }
 
-pub trait Linked {
-    type FromEntity: EntityTrait;
-
-    type ToEntity: EntityTrait;
-
-    fn link(&self) -> Vec<RelationDef>;
-
-    fn find_linked(&self) -> Select<Self::ToEntity> {
-        let mut select = Select::new();
-        for rel in self.link().into_iter().rev() {
-            select = select.join_rev(JoinType::InnerJoin, rel);
-        }
-        select
-    }
-}
-
 #[derive(Debug)]
 pub struct RelationDef {
     pub rel_type: RelationType,
