605058f2d855af9a0aa34ac15e9268567dea3333
Marco Napetti <nappa85@therocktrading.com>
2021-09-21 14:58:16 +0200

    use ouroboros to cover self-references

--- src/database/stream.rs
+++ src/database/stream.rs
@@ -1,4 +1,4 @@
-use std::{pin::Pin, sync::Arc, task::Poll};
+use std::{pin::Pin, task::Poll};
 
 use futures::{Stream, TryStreamExt};
 
@@ -15,42 +15,33 @@ enum Connection {
     Sqlite(PoolConnection<sqlx::Sqlite>),
 }
 
+#[ouroboros::self_referencing]
 pub struct QueryStream {
-    stmt: Arc<Statement>,
-    conn: Arc<Connection>,
-    stream: Option<Pin<Box<dyn Stream<Item = Result<QueryResult, DbErr>>>>>,
+    stmt: Statement,
+    conn: Connection,
+    #[borrows(mut conn, stmt)]
+    #[not_covariant]
+    stream: Pin<Box<dyn Stream<Item = Result<QueryResult, DbErr>> + 'this>>,
 }
 
 #[cfg(feature = "sqlx-mysql")]
 impl From<(PoolConnection<sqlx::MySql>, Statement)> for QueryStream {
     fn from((conn, stmt): (PoolConnection<sqlx::MySql>, Statement)) -> Self {
-        QueryStream {
-            stmt: Arc::new(stmt),
-            conn: Arc::new(Connection::MySql(conn)),
-            stream: None
-        }
+        QueryStream::build(stmt, Connection::MySql(conn))
     }
 }
 
 #[cfg(feature = "sqlx-postgres")]
 impl From<(PoolConnection<sqlx::Postgres>, Statement)> for QueryStream {
     fn from((conn, stmt): (PoolConnection<sqlx::Postgres>, Statement)) -> Self {
-        QueryStream {
-            stmt: Arc::new(stmt),
-            conn: Arc::new(Connection::Postgres(conn)),
-            stream: None
-        }
+        QueryStream::build(stmt, Connection::Postgres(conn))
     }
 }
 
 #[cfg(feature = "sqlx-sqlite")]
 impl From<(PoolConnection<sqlx::Sqlite>, Statement)> for QueryStream {
     fn from((conn, stmt): (PoolConnection<sqlx::Sqlite>, Statement)) -> Self {
-        QueryStream {
-            stmt: Arc::new(stmt),
-            conn: Arc::new(Connection::Sqlite(conn)),
-            stream: None
-        }
+        QueryStream::build(stmt, Connection::Sqlite(conn))
     }
 }
 
@@ -61,45 +52,42 @@ impl std::fmt::Debug for QueryStream {
 }
 
 impl QueryStream {
-    fn get_conn(&mut self) -> Option<&'static mut Connection> {
-        // this is safe since the connection is owned and the stream, that references the connection, is owned too, so they die tougheter
-        unsafe { std::mem::transmute(Arc::get_mut(&mut self.conn)) }
-    }
-    fn get_stmt(&self) -> &'static Statement {
-        // this is safe since the statement is owned and the stream, that references the statement, is owned too, so they die tougheter
-        unsafe { std::mem::transmute(self.stmt.as_ref()) }
-    }
-    fn init(&mut self) {
-        match self.get_conn() {
-            #[cfg(feature = "sqlx-mysql")]
-            Some(Connection::MySql(c)) => {
-                let query = crate::driver::sqlx_mysql::sqlx_query(self.get_stmt());
-                self.stream = Some(Box::pin(
-                    c.fetch(query)
-                        .map_ok(Into::into)
-                        .map_err(sqlx_error_to_query_err)
-                ));
+    fn build(stmt: Statement, conn: Connection) -> Self {
+        QueryStreamBuilder {
+            stmt,
+            conn,
+            stream_builder: |conn, stmt| {
+                match conn {
+                    #[cfg(feature = "sqlx-mysql")]
+                    Connection::MySql(c) => {
+                        let query = crate::driver::sqlx_mysql::sqlx_query(stmt);
+                        Box::pin(
+                            c.fetch(query)
+                                .map_ok(Into::into)
+                                .map_err(sqlx_error_to_query_err)
+                        )
+                    },
+                    #[cfg(feature = "sqlx-postgres")]
+                    Connection::Postgres(c) => {
+                        let query = crate::driver::sqlx_postgres::sqlx_query(stmt);
+                        Box::pin(
+                            c.fetch(query)
+                                .map_ok(Into::into)
+                                .map_err(sqlx_error_to_query_err)
+                        )
+                    },
+                    #[cfg(feature = "sqlx-sqlite")]
+                    Connection::Sqlite(c) => {
+                        let query = crate::driver::sqlx_sqlite::sqlx_query(stmt);
+                        Box::pin(
+                            c.fetch(query)
+                                .map_ok(Into::into)
+                                .map_err(sqlx_error_to_query_err)
+                        )
+                    },
+                }
             },
-            #[cfg(feature = "sqlx-postgres")]
-            Some(Connection::Postgres(c)) => {
-                let query = crate::driver::sqlx_postgres::sqlx_query(self.get_stmt());
-                self.stream = Some(Box::pin(
-                    c.fetch(query)
-                        .map_ok(Into::into)
-                        .map_err(sqlx_error_to_query_err)
-                ));
-            },
-            #[cfg(feature = "sqlx-sqlite")]
-            Some(Connection::Sqlite(c)) => {
-                let query = crate::driver::sqlx_sqlite::sqlx_query(self.get_stmt());
-                self.stream = Some(Box::pin(
-                    c.fetch(query)
-                        .map_ok(Into::into)
-                        .map_err(sqlx_error_to_query_err)
-                ));
-            },
-            _ => unreachable!(),
-        }
+        }.build()
     }
 }
 
@@ -108,14 +96,8 @@ impl Stream for QueryStream {
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> Poll<Option<Self::Item>> {
         let this = self.get_mut();
-        if this.stream.is_none() {
-            this.init();
-        }
-        if let Some(stream) = this.stream.as_mut() {
+        this.with_stream_mut(|stream| {
             stream.as_mut().poll_next(cx)
-        }
-        else {
-            unreachable!();
-        }
+        })
     }
 }
