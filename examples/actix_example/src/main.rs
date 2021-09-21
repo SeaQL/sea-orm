f7b42ebc0e6e0302223e3bb247b0b9b8585eadda
Chris Tsang <tyt2y7@gmail.com>
2021-09-21 18:04:26 +0800

    Clean up Cargo.toml

--- examples/actix_example/src/main.rs
+++ examples/actix_example/src/main.rs
@@ -179,7 +179,7 @@ async fn delete(
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    std::env::set_var("RUST_LOG", "actix_web=debug");
+    std::env::set_var("RUST_LOG", "debug");
     env_logger::init();
 
     // get env vars
