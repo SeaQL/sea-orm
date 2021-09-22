9f77a5fc28d7fa789b016f27ef8633c9dc7ccd03
Chris Tsang <tyt2y7@gmail.com>
2021-09-22 18:00:25 +0800

    Improve build script

--- build-tools/cargo-publish.sh
+++ build-tools/cargo-publish.sh
@@ -5,15 +5,18 @@ sed -i 's/^version.*$/version = "'$1'"/' Cargo.toml
 git commit -am "sea-orm-codegen $1"
 cargo publish
 cd ..
+sleep 10
 cd sea-orm-cli
 sed -i 's/^version.*$/'"version = \"$1\"/" Cargo.toml
 sed -i 's/^sea-orm-codegen [^,]*,/sea-orm-codegen = { version = "\^'$1'",/' Cargo.toml
 git commit -am "sea-orm-cli $1"
 cargo publish
 cd ..
+sleep 10
 cd sea-orm-macros
 sed -i 's/^version.*$/version = "'$1'"/' Cargo.toml
 git commit -am "sea-orm-macros $1"
 cargo publish
 cd ..
+sed -i 's/^version.*$/version = "'$1'"/' Cargo.toml
 sed -i 's/^sea-orm-macros [^,]*,/sea-orm-macros = { version = "\^'$1'",/' Cargo.toml
