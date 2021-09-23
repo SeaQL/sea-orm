8f0852958ffe216ab75c276063852c1d30da2dd4
baoyachi <liaoymxsdl@gmail.com>
2021-09-23 21:30:53 +0800

    refactor code with macro debug_query! ref:https://github.com/SeaQL/sea-orm/issues/145

--- src/query/traits.rs
+++ src/query/traits.rs
@@ -23,14 +23,99 @@ pub trait QueryTrait {
     }
 }
 
+#[derive(Debug)]
+pub struct DebugQuery<'a, Q, T> {
+    pub query: &'a Q,
+    pub value: T,
+}
+
+impl<'a, Q> DebugQuery<'a, Q, DbBackend>
+where
+    Q: QueryTrait,
+{
+    pub fn build(&self) -> Statement {
+        self.query.build(self.value)
+    }
+}
+
+impl<'a, Q> DebugQuery<'a, Q, &DatabaseConnection>
+where
+    Q: QueryTrait,
+{
+    pub fn build(&self) -> Statement {
+        self.query.build(self.value.get_database_backend())
+    }
+}
+
 /// Make get raw_sql becomes simply. It does not need to specify a specific `DbBackend`,
 /// but can be obtained through `get_database_backend` with `DatabaseConnection`.
 /// Return a Statement type.
-pub fn debug_query(query: &impl QueryTrait, conn: &DatabaseConnection) -> Statement {
-    query.build(conn.get_database_backend())
+///
+///
+/// # Example
+///
+/// ```
+/// # #[cfg(feature = "mock")]
+/// # use sea_orm::{error::*, tests_cfg::*, MockDatabase, MockExecResult, Transaction, DbBackend};
+/// #
+/// # let conn = MockDatabase::new(DbBackend::Postgres)
+/// #     .into_connection();
+/// #
+/// use sea_orm::{entity::*, query::*, tests_cfg::cake,debug_query};
+///
+/// let c = cake::Entity::insert(
+///    cake::ActiveModel {
+///         id: ActiveValue::set(1),
+///         name: ActiveValue::set("Apple Pie".to_owned()),
+/// });
+///
+/// let raw_sql = debug_query!(&c,&conn).to_string();
+/// assert_eq!(raw_sql,r#"INSERT INTO "cake" ("id", "name") VALUES (1, 'Apple Pie')"#);
+///
+/// let raw_sql = debug_query!(&c,DbBackend::MySql).to_string();
+/// assert_eq!(raw_sql,r#"INSERT INTO `cake` (`id`, `name`) VALUES (1, 'Apple Pie')"#);
+///
+/// ```
+#[macro_export]
+macro_rules! debug_query {
+    ($query:expr,$value:expr) => {
+        $crate::DebugQuery {
+            query: $query,
+            value: $value,
+        }
+        .build();
+    };
 }
 
 /// Use `debug_query` get raw_sql.
-pub fn debug_query_fmt(query: &impl QueryTrait, conn: &DatabaseConnection) -> String {
-    debug_query(query, conn).to_string()
+///
+/// # Example
+///
+/// ```
+/// # #[cfg(feature = "mock")]
+/// # use sea_orm::{error::*, tests_cfg::*, MockDatabase, MockExecResult, Transaction, DbBackend};
+/// #
+/// # let conn = MockDatabase::new(DbBackend::Postgres)
+/// #     .into_connection();
+/// #
+/// use sea_orm::{entity::*, query::*, tests_cfg::cake,debug_query_fmt};
+///
+/// let c = cake::Entity::insert(
+///    cake::ActiveModel {
+///         id: ActiveValue::set(1),
+///         name: ActiveValue::set("Apple Pie".to_owned()),
+/// });
+///
+/// let raw_sql = debug_query_fmt!(&c,&conn);
+/// assert_eq!(raw_sql,r#"INSERT INTO "cake" ("id", "name") VALUES (1, 'Apple Pie')"#);
+///
+/// let raw_sql = debug_query_fmt!(&c,DbBackend::Sqlite);
+/// assert_eq!(raw_sql,r#"INSERT INTO `cake` (`id`, `name`) VALUES (1, 'Apple Pie')"#);
+///
+/// ```
+#[macro_export]
+macro_rules! debug_query_fmt {
+    ($query:expr,$value:expr) => {
+        $crate::debug_query!($query, $value).to_string();
+    };
 }
