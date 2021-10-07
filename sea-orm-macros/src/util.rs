cab0f6ba6e5ef5421aa8f15e109fd93898d7336c
Chris Tsang <chris.2y3@outlook.com>
2021-10-07 16:35:50 +0800

    Merge pull request #224 from SeaQL/rust-keywords
    Escape rust keywords with `r#` raw identifier

--- sea-orm-macros/src/util.rs
+++ sea-orm-macros/src/util.rs
@@ -24,3 +24,39 @@ pub(crate) fn field_not_ignored(field: &Field) -> bool {
     }
     true
 }
+
+pub(crate) fn trim_starting_raw_identifier<T>(string: T) -> String
+where
+    T: ToString,
+{
+    string
+        .to_string()
+        .trim_start_matches(RAW_IDENTIFIER)
+        .to_string()
+}
+
+pub(crate) fn escape_rust_keyword<T>(string: T) -> String
+where
+    T: ToString,
+{
+    let string = string.to_string();
+    if RUST_KEYWORDS.iter().any(|s| s.eq(&string)) {
+        format!("r#{}", string)
+    } else if RUST_SPECIAL_KEYWORDS.iter().any(|s| s.eq(&string)) {
+        format!("{}_", string)
+    } else {
+        string
+    }
+}
+
+pub(crate) const RAW_IDENTIFIER: &str = "r#";
+
+pub(crate) const RUST_KEYWORDS: [&str; 49] = [
+    "as", "async", "await", "break", "const", "continue", "dyn", "else", "enum", "extern", "false",
+    "fn", "for", "if", "impl", "in", "let", "loop", "match", "mod", "move", "mut", "pub", "ref",
+    "return", "static", "struct", "super", "trait", "true", "type", "union", "unsafe", "use",
+    "where", "while", "abstract", "become", "box", "do", "final", "macro", "override", "priv",
+    "try", "typeof", "unsized", "virtual", "yield",
+];
+
+pub(crate) const RUST_SPECIAL_KEYWORDS: [&str; 3] = ["crate", "Self", "self"];
