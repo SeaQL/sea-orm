b5c18cab55de4fdc52577f63ca1cd05b0a01c232
Chris Tsang <tyt2y7@gmail.com>
2021-10-16 01:19:33 +0800

    Sleep longer

--- build-tools/cargo-publish.sh
+++ build-tools/cargo-publish.sh
@@ -5,14 +5,14 @@ sed -i 's/^version.*$/version = "'$1'"/' Cargo.toml
 git commit -am "sea-orm-codegen $1"
 cargo publish
 cd ..
-sleep 10
+sleep 30
 cd sea-orm-cli
 sed -i 's/^version.*$/'"version = \"$1\"/" Cargo.toml
 sed -i 's/^sea-orm-codegen [^,]*,/sea-orm-codegen = { version = "\^'$1'",/' Cargo.toml
 git commit -am "sea-orm-cli $1"
 cargo publish
 cd ..
-sleep 10
+sleep 30
 cd sea-orm-macros
 sed -i 's/^version.*$/version = "'$1'"/' Cargo.toml
 git commit -am "sea-orm-macros $1"
