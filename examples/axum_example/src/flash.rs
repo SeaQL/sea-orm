666a5bb06863f89a9cbecd6e5564bee033b8335b
Billy Chan <ccw.billy.123@gmail.com>
2021-11-08 12:50:38 +0800

    cargo fmt

--- examples/axum_example/src/flash.rs
+++ examples/axum_example/src/flash.rs
@@ -20,13 +20,13 @@ pub fn get_flash_cookie<T>(cookies: &Cookies) -> Option<T>
 where
     T: DeserializeOwned,
 {
-    cookies.get(FLASH_COOKIE_NAME).and_then(|flash_cookie|
+    cookies.get(FLASH_COOKIE_NAME).and_then(|flash_cookie| {
         (if let Ok(ValuedMessage::<T> { value }) = serde_json::from_str(flash_cookie.value()) {
             Some(value)
         } else {
             None
-        }
-    ))
+        })
+    })
 }
 
 pub type PostResponse = (StatusCode, HeaderMap);
