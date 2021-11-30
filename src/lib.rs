1290becd763b4eab56c18c722160f500e64834d5
Billy Chan <ccw.billy.123@gmail.com>
2021-11-30 19:00:19 +0800

    Update README

--- src/lib.rs
+++ src/lib.rs
@@ -184,7 +184,7 @@
 //! pear.name = Set("Sweet pear".to_owned());
 //!
 //! // update one
-//! let pear: fruit::ActiveModel = pear.update(db).await?;
+//! let pear: fruit::Model = pear.update(db).await?;
 //!
 //! // update many: UPDATE "fruit" SET "cake_id" = NULL WHERE "fruit"."name" LIKE '%Apple%'
 //! Fruit::update_many()
@@ -207,7 +207,7 @@
 //! };
 //!
 //! // create, because primary key `id` is `Unset`
-//! let mut banana = banana.save(db).await?;
+//! let mut banana = banana.save(db).await?.into_active_model();
 //!
 //! banana.name = Set("Banana Mongo".to_owned());
 //!
