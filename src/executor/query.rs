1229287fd83345b658e215d72ad1c38f84c463fd
Billy Chan <30400950+billy1624@users.noreply.github.com>
2021-12-04 20:57:05 +0800

    Support Up to 6 Values Composite Primary Key (#353)
    * Support up to 6 composite primary key
    
    * Test [issues]
    
    * Test [issues]

--- src/executor/query.rs
+++ src/executor/query.rs
@@ -451,6 +451,48 @@ where
     }
 }
 
+impl<A, B, C, D, E> TryGetableMany for (A, B, C, D, E)
+where
+    A: TryGetable,
+    B: TryGetable,
+    C: TryGetable,
+    D: TryGetable,
+    E: TryGetable,
+{
+    fn try_get_many(res: &QueryResult, pre: &str, cols: &[String]) -> Result<Self, TryGetError> {
+        try_get_many_with_slice_len_of(5, cols)?;
+        Ok((
+            A::try_get(res, pre, &cols[0])?,
+            B::try_get(res, pre, &cols[1])?,
+            C::try_get(res, pre, &cols[2])?,
+            D::try_get(res, pre, &cols[3])?,
+            E::try_get(res, pre, &cols[4])?,
+        ))
+    }
+}
+
+impl<A, B, C, D, E, F> TryGetableMany for (A, B, C, D, E, F)
+where
+    A: TryGetable,
+    B: TryGetable,
+    C: TryGetable,
+    D: TryGetable,
+    E: TryGetable,
+    F: TryGetable,
+{
+    fn try_get_many(res: &QueryResult, pre: &str, cols: &[String]) -> Result<Self, TryGetError> {
+        try_get_many_with_slice_len_of(6, cols)?;
+        Ok((
+            A::try_get(res, pre, &cols[0])?,
+            B::try_get(res, pre, &cols[1])?,
+            C::try_get(res, pre, &cols[2])?,
+            D::try_get(res, pre, &cols[3])?,
+            E::try_get(res, pre, &cols[4])?,
+            F::try_get(res, pre, &cols[5])?,
+        ))
+    }
+}
+
 fn try_get_many_with_slice_len_of(len: usize, cols: &[String]) -> Result<(), TryGetError> {
     if cols.len() < len {
         Err(TryGetError::DbErr(DbErr::Query(format!(
@@ -501,6 +543,8 @@ macro_rules! try_from_u64_err {
 try_from_u64_err!(A, B);
 try_from_u64_err!(A, B, C);
 try_from_u64_err!(A, B, C, D);
+try_from_u64_err!(A, B, C, D, E);
+try_from_u64_err!(A, B, C, D, E, F);
 
 macro_rules! try_from_u64_numeric {
     ( $type: ty ) => {
@@ -540,10 +584,28 @@ macro_rules! try_from_u64_string {
 
 try_from_u64_string!(String);
 
+try_from_u64_err!(bool);
+try_from_u64_err!(f32);
+try_from_u64_err!(f64);
 try_from_u64_err!(Vec<u8>);
 
-#[cfg(feature = "with-uuid")]
-try_from_u64_err!(uuid::Uuid);
+#[cfg(feature = "with-json")]
+try_from_u64_err!(serde_json::Value);
+
+#[cfg(feature = "with-chrono")]
+try_from_u64_err!(chrono::NaiveDate);
+
+#[cfg(feature = "with-chrono")]
+try_from_u64_err!(chrono::NaiveTime);
+
+#[cfg(feature = "with-chrono")]
+try_from_u64_err!(chrono::NaiveDateTime);
 
 #[cfg(feature = "with-chrono")]
 try_from_u64_err!(chrono::DateTime<chrono::FixedOffset>);
+
+#[cfg(feature = "with-rust_decimal")]
+try_from_u64_err!(rust_decimal::Decimal);
+
+#[cfg(feature = "with-uuid")]
+try_from_u64_err!(uuid::Uuid);
