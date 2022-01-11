3cb5fa7992986d6cf3db147e0617125dd6590698
Billy Chan <ccw.billy.123@gmail.com>
2022-01-12 00:45:28 +0800

    test: fix test cases, assert panic message

--- sea-orm-cli/src/main.rs
+++ sea-orm-cli/src/main.rs
@@ -198,12 +198,11 @@ where
 #[cfg(test)]
 mod tests {
     use clap::AppSettings;
-    use url::ParseError;
-
     use super::*;
 
-    #[async_std::test]
-    async fn test_generate_entity_no_protocol() {
+    #[test]
+    #[should_panic(expected = "called `Result::unwrap()` on an `Err` value: RelativeUrlWithoutBase")]
+    fn test_generate_entity_no_protocol() {
         let matches = cli::build_cli()
             .setting(AppSettings::NoBinaryName)
             .get_matches_from(vec![
@@ -213,22 +212,11 @@ mod tests {
                 "://root:root@localhost:3306/database",
             ]);
 
-        let result = std::panic::catch_unwind(|| {
-            smol::block_on(run_generate_command(matches.subcommand().1.unwrap()))
-        });
-
-        // Make sure result is a ParseError
-        match result {
-            Ok(Err(e)) => match e.downcast::<ParseError>() {
-                Ok(_) => (),
-                Err(e) => panic!("Expected ParseError but got: {:?}", e),
-            },
-            _ => panic!("Should have panicked"),
-        }
+        smol::block_on(run_generate_command(matches.subcommand().1.unwrap())).unwrap();
     }
 
     #[test]
-    #[should_panic]
+    #[should_panic(expected = "There is no database name as part of the url path: postgresql://root:root@localhost:3306")]
     fn test_generate_entity_no_database_section() {
         let matches = cli::build_cli()
             .setting(AppSettings::NoBinaryName)
@@ -239,12 +227,11 @@ mod tests {
                 "postgresql://root:root@localhost:3306",
             ]);
 
-        smol::block_on(run_generate_command(matches.subcommand().1.unwrap()))
-            .unwrap_or_else(handle_error);
+        smol::block_on(run_generate_command(matches.subcommand().1.unwrap())).unwrap();
     }
 
     #[test]
-    #[should_panic]
+    #[should_panic(expected = "There is no database name as part of the url path: mysql://root:root@localhost:3306/")]
     fn test_generate_entity_no_database_path() {
         let matches = cli::build_cli()
             .setting(AppSettings::NoBinaryName)
@@ -255,12 +242,11 @@ mod tests {
                 "mysql://root:root@localhost:3306/",
             ]);
 
-        smol::block_on(run_generate_command(matches.subcommand().1.unwrap()))
-            .unwrap_or_else(handle_error);
+        smol::block_on(run_generate_command(matches.subcommand().1.unwrap())).unwrap();
     }
 
     #[test]
-    #[should_panic]
+    #[should_panic(expected = "No username was found in the database url")]
     fn test_generate_entity_no_username() {
         let matches = cli::build_cli()
             .setting(AppSettings::NoBinaryName)
@@ -271,11 +257,11 @@ mod tests {
                 "mysql://:root@localhost:3306/database",
             ]);
 
-        smol::block_on(run_generate_command(matches.subcommand().1.unwrap()))
-            .unwrap_or_else(handle_error);
+        smol::block_on(run_generate_command(matches.subcommand().1.unwrap())).unwrap();
     }
 
     #[test]
+    #[should_panic(expected = "called `Result::unwrap()` on an `Err` value: PoolTimedOut")]
     fn test_generate_entity_no_password() {
         let matches = cli::build_cli()
             .setting(AppSettings::NoBinaryName)
@@ -286,12 +272,12 @@ mod tests {
                 "mysql://root:@localhost:3306/database",
             ]);
 
-        smol::block_on(run_generate_command(matches.subcommand().1.unwrap()))
-            .unwrap_or_else(handle_error);
+        smol::block_on(run_generate_command(matches.subcommand().1.unwrap())).unwrap();
     }
 
-    #[async_std::test]
-    async fn test_generate_entity_no_host() {
+    #[test]
+    #[should_panic(expected = "called `Result::unwrap()` on an `Err` value: EmptyHost")]
+    fn test_generate_entity_no_host() {
         let matches = cli::build_cli()
             .setting(AppSettings::NoBinaryName)
             .get_matches_from(vec![
@@ -301,17 +287,6 @@ mod tests {
                 "postgres://root:root@/database",
             ]);
 
-        let result = std::panic::catch_unwind(|| {
-            smol::block_on(run_generate_command(matches.subcommand().1.unwrap()))
-        });
-
-        // Make sure result is a ParseError
-        match result {
-            Ok(Err(e)) => match e.downcast::<ParseError>() {
-                Ok(_) => (),
-                Err(e) => panic!("Expected ParseError but got: {:?}", e),
-            },
-            _ => panic!("Should have panicked"),
-        }
+        smol::block_on(run_generate_command(matches.subcommand().1.unwrap())).unwrap();
     }
 }
