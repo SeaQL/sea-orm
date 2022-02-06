a860577af21a1a159215f04fba41d191ec41a748
Chris Tsang <chris.2y3@outlook.com>
2022-02-06 14:19:47 +0800

    Use sea-query 0.21

--- build-tools/cargo-publish.sh
+++ build-tools/cargo-publish.sh
@@ -6,13 +6,13 @@ git commit -am "sea-orm-codegen $1"
 cargo publish
 cd ..
 sleep 30
-cd sea-orm-cli
-sed -i 's/^version.*$/'"version = \"$1\"/" Cargo.toml
-sed -i 's/^sea-orm-codegen [^,]*,/sea-orm-codegen = { version = "\^'$1'",/' Cargo.toml
-git commit -am "sea-orm-cli $1"
-cargo publish
-cd ..
-sleep 30
+# cd sea-orm-cli
+# sed -i 's/^version.*$/'"version = \"$1\"/" Cargo.toml
+# sed -i 's/^sea-orm-codegen [^,]*,/sea-orm-codegen = { version = "\^'$1'",/' Cargo.toml
+# git commit -am "sea-orm-cli $1"
+# cargo publish
+# cd ..
+# sleep 30
 cd sea-orm-macros
 sed -i 's/^version.*$/version = "'$1'"/' Cargo.toml
 git commit -am "sea-orm-macros $1"
