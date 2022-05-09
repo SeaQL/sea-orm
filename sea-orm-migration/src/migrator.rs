07c8af2b3a8a62f7d730ddb4c36e21bf64f5bdff
Chris Tsang <chris.2y3@outlook.com>
2022-05-09 17:04:12 +0800

    Use sea-schema's SchemaProbe

--- sea-orm-migration/src/migrator.rs
+++ sea-orm-migration/src/migrator.rs
@@ -1,14 +1,15 @@
-use super::{seaql_migrations, MigrationTrait, SchemaManager};
-use sea_orm::sea_query::{
-    Alias, Expr, ForeignKey, IntoTableRef, Query, SelectStatement, SimpleExpr, Table,
-};
+use std::fmt::Display;
+use std::time::SystemTime;
+use tracing::info;
+
+use sea_orm::sea_query::{Alias, Expr, ForeignKey, Query, SelectStatement, SimpleExpr, Table};
 use sea_orm::{
     ActiveModelTrait, ActiveValue, ColumnTrait, Condition, ConnectionTrait, DbBackend, DbConn,
     DbErr, EntityTrait, QueryFilter, QueryOrder, Schema, Statement,
 };
-use std::fmt::Display;
-use std::time::SystemTime;
-use tracing::info;
+use sea_schema::{mysql::MySql, postgres::Postgres, probe::SchemaProbe, sqlite::Sqlite};
+
+use super::{seaql_migrations, MigrationTrait, SchemaManager};
 
 #[derive(Debug, PartialEq)]
 /// Status of migration
@@ -291,44 +292,17 @@ pub trait MigratorTrait: Send {
 }
 
 pub(crate) fn query_tables(db: &DbConn) -> SelectStatement {
-    let mut stmt = Query::select();
-    let (expr, tbl_ref, condition) = match db.get_database_backend() {
-        DbBackend::MySql => (
-            Expr::col(Alias::new("table_name")),
-            (Alias::new("information_schema"), Alias::new("tables")).into_table_ref(),
-            Condition::all().add(
-                Expr::expr(get_current_schema(db))
-                    .equals(Alias::new("tables"), Alias::new("table_schema")),
-            ),
-        ),
-        DbBackend::Postgres => (
-            Expr::col(Alias::new("table_name")),
-            (Alias::new("information_schema"), Alias::new("tables")).into_table_ref(),
-            Condition::all()
-                .add(
-                    Expr::expr(get_current_schema(db))
-                        .equals(Alias::new("tables"), Alias::new("table_schema")),
-                )
-                .add(Expr::col(Alias::new("table_type")).eq("BASE TABLE")),
-        ),
-        DbBackend::Sqlite => (
-            Expr::col(Alias::new("name")),
-            Alias::new("sqlite_master").into_table_ref(),
-            Condition::all()
-                .add(Expr::col(Alias::new("type")).eq("table"))
-                .add(Expr::col(Alias::new("name")).ne("sqlite_sequence")),
-        ),
-    };
-    stmt.expr_as(expr, Alias::new("table_name"))
-        .from(tbl_ref)
-        .cond_where(condition);
-    stmt
+    match db.get_database_backend() {
+        DbBackend::MySql => MySql::query_tables(),
+        DbBackend::Postgres => Postgres::query_tables(),
+        DbBackend::Sqlite => Sqlite::query_tables(),
+    }
 }
 
 pub(crate) fn get_current_schema(db: &DbConn) -> SimpleExpr {
     match db.get_database_backend() {
-        DbBackend::MySql => Expr::cust("DATABASE()"),
-        DbBackend::Postgres => Expr::cust("CURRENT_SCHEMA()"),
+        DbBackend::MySql => MySql::get_current_schema(),
+        DbBackend::Postgres => Postgres::get_current_schema(),
         DbBackend::Sqlite => unimplemented!(),
     }
 }
