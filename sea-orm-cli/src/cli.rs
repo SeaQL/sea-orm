ba16b8f649a8150fc5bc4e67820a11d89cf9e87e
Nahua Kang <kangnahua@gmail.com>
2022-05-13 10:12:54 +0200

    Add date-time-crate flag for cli

--- sea-orm-cli/src/cli.rs
+++ sea-orm-cli/src/cli.rs
@@ -1,150 +1,27 @@
 use clap::{ArgEnum, ArgGroup, Parser, Subcommand};
 
-#[derive(Parser, Debug)]
-#[clap(version)]
-pub struct Cli {
-    #[clap(action, global = true, short, long, help = "Show debug messages")]
-    pub verbose: bool,
-
-    #[clap(subcommand)]
-    pub command: Commands,
-}
-
-#[derive(Subcommand, PartialEq, Eq, Debug)]
-pub enum Commands {
-    #[clap(about = "Codegen related commands")]
-    #[clap(arg_required_else_help = true)]
-    Generate {
-        #[clap(subcommand)]
-        command: GenerateSubcommands,
-    },
-    #[clap(about = "Migration related commands")]
-    Migrate {
-        #[clap(
-            value_parser,
-            global = true,
-            short = 'd',
-            long,
-            help = "Migration script directory",
-            default_value = "./migration"
-        )]
-        migration_dir: String,
-
-        #[clap(subcommand)]
-        command: Option<MigrateSubcommands>,
-    },
-}
-
-#[derive(Subcommand, PartialEq, Eq, Debug)]
-pub enum MigrateSubcommands {
-    #[clap(about = "Initialize migration directory")]
-    Init,
-    #[clap(about = "Generate a new, empty migration")]
-    Generate {
-        #[clap(
-            value_parser,
-            required = true,
-            takes_value = true,
-            help = "Name of the new migration"
-        )]
-        migration_name: String,
-    },
-    #[clap(about = "Drop all tables from the database, then reapply all migrations")]
-    Fresh,
-    #[clap(about = "Rollback all applied migrations, then reapply all migrations")]
-    Refresh,
-    #[clap(about = "Rollback all applied migrations")]
-    Reset,
-    #[clap(about = "Check the status of all migrations")]
-    Status,
-    #[clap(about = "Apply pending migrations")]
-    Up {
-        #[clap(
-            value_parser,
-            short,
-            long,
-            default_value = "1",
-            help = "Number of pending migrations to apply"
-        )]
-        num: u32,
-    },
-    #[clap(value_parser, about = "Rollback applied migrations")]
-    Down {
-        #[clap(
-            value_parser,
-            short,
-            long,
-            default_value = "1",
-            help = "Number of applied migrations to be rolled back"
-        )]
-        num: u32,
-    },
-}
-
-#[derive(Subcommand, PartialEq, Eq, Debug)]
-pub enum GenerateSubcommands {
-    #[clap(about = "Generate entity")]
-    #[clap(arg_required_else_help = true)]
-    #[clap(group(ArgGroup::new("formats").args(&["compact-format", "expanded-format"])))]
-    #[clap(group(ArgGroup::new("group-tables").args(&["tables", "include-hidden-tables"])))]
-    Entity {
-        #[clap(action, long, help = "Generate entity file of compact format")]
-        compact_format: bool,
-
-        #[clap(action, long, help = "Generate entity file of expanded format")]
-        expanded_format: bool,
-
-        #[clap(
-            action,
-            long,
-            help = "Generate entity file for hidden tables (i.e. table name starts with an underscore)"
-        )]
-        include_hidden_tables: bool,
-
-        #[clap(
-            value_parser,
-            short = 't',
-            long,
-            use_value_delimiter = true,
-            takes_value = true,
-            help = "Generate entity file for specified tables only (comma separated)"
-        )]
-        tables: Option<String>,
-
-        #[clap(
-            value_parser,
-            long,
-            use_value_delimiter = true,
-            takes_value = true,
-            default_value = "seaql_migrations",
-            help = "Skip generating entity file for specified tables (comma separated)"
-        )]
-        ignore_tables: Vec<String>,
-
-        #[clap(
-            value_parser,
-            long,
-            default_value = "1",
-            help = "The maximum amount of connections to use when connecting to the database."
-        )]
-        max_connections: u32,
-
-        #[clap(
-            value_parser,
-            short = 'o',
-            long,
-            default_value = "./",
-            help = "Entity file output directory"
-        )]
-        output_dir: String,
-
-        #[clap(
-            value_parser,
-            short = 's',
-            long,
-            env = "DATABASE_SCHEMA",
-            default_value = "public",
-            long_help = "Database schema\n \
+pub fn build_cli() -> App<'static, 'static> {
+    let entity_subcommand = SubCommand::with_name("generate")
+        .about("Codegen related commands")
+        .setting(AppSettings::VersionlessSubcommands)
+        .subcommand(
+            SubCommand::with_name("entity")
+                .about("Generate entity")
+                .arg(
+                    Arg::with_name("DATABASE_URL")
+                        .long("database-url")
+                        .short("u")
+                        .help("Database URL")
+                        .takes_value(true)
+                        .required(true)
+                        .env("DATABASE_URL"),
+                )
+                .arg(
+                    Arg::with_name("DATABASE_SCHEMA")
+                        .long("database-schema")
+                        .short("s")
+                        .help("Database schema")
+                        .long_help("Database schema\n \
                         - For MySQL, this argument is ignored.\n \
                         - For PostgreSQL, this argument is optional with default value 'public'."
         )]
@@ -186,16 +63,35 @@ pub enum GenerateSubcommands {
         )]
         singularize: bool,
 
-        #[clap(
-            arg_enum,
-            value_parser,
-            long,
-            default_value = "chrono",
-            help = "The datetime crate to use for generating entities."
-        )]
-        date_time_crate: DateTimeCrate,
-    },
-}
+    let arg_migration_dir = Arg::with_name("MIGRATION_DIR")
+        .long("migration-dir")
+        .short("d")
+        .help("Migration script directory")
+        .takes_value(true)
+        .default_value("./migration");
+    let mut migrate_subcommands = SubCommand::with_name("migrate")
+        .about("Migration related commands")
+        .subcommand(
+            SubCommand::with_name("init")
+                .about("Initialize migration directory")
+                .arg(arg_migration_dir.clone()),
+        )
+        .subcommand(
+            SubCommand::with_name("generate")
+                .about("Generate a new, empty migration")
+                .arg(
+                    Arg::with_name("MIGRATION_NAME")
+                        .help("Name of the new migation")
+                        .required(true)
+                        .takes_value(true),
+                )
+                .arg(arg_migration_dir.clone()),
+        )
+        .arg(arg_migration_dir.clone());
+    for subcommand in get_subcommands() {
+        migrate_subcommands =
+            migrate_subcommands.subcommand(subcommand.arg(arg_migration_dir.clone()));
+    }
 
 #[derive(ArgEnum, Copy, Clone, Debug, PartialEq, Eq)]
 pub enum DateTimeCrate {
