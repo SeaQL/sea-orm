9b41f1c1b17917626d204b39da3f7411a4855fde
Billy Chan <ccw.billy.123@gmail.com>
2022-06-12 22:25:32 +0800

    Migration shouldn't depends on symbol provided by entity crate (#785)
    * Migration shouldn't depends on symbol provided by entity crate
    
    * Add docs

--- examples/graphql_example/migration/src/m20220101_000001_create_table.rs
+++ examples/graphql_example/migration/src/m20220101_000001_create_table.rs
@@ -1,21 +1,6 @@
-use entity::note;
-use sea_orm::{DbBackend, EntityTrait, Schema};
 use sea_orm_migration::prelude::*;
 
 pub struct Migration;
-
-fn get_seaorm_create_stmt<E: EntityTrait>(e: E) -> TableCreateStatement {
-    let schema = Schema::new(DbBackend::Sqlite);
-
-    schema
-        .create_table_from_entity(e)
-        .if_not_exists()
-        .to_owned()
-}
-
-fn get_seaorm_drop_stmt<E: EntityTrait>(e: E) -> TableDropStatement {
-    Table::drop().table(e).if_exists().to_owned()
-}
 
 impl MigrationName for Migration {
     fn name(&self) -> &str {
@@ -26,22 +11,42 @@ impl MigrationName for Migration {
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        let stmts = vec![get_seaorm_create_stmt(note::Entity)];
-
-        for stmt in stmts {
-            manager.create_table(stmt.to_owned()).await?;
-        }
-
-        Ok(())
+        manager
+            .create_table(
+                Table::create()
+                    .table(Notes::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(Notes::Id)
+                            .integer()
+                            .not_null()
+                            .auto_increment()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(Notes::Title).string().not_null())
+                    .col(ColumnDef::new(Notes::Text).string().not_null())
+                    .to_owned(),
+            )
+            .await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        let stmts = vec![get_seaorm_drop_stmt(note::Entity)];
-
-        for stmt in stmts {
-            manager.drop_table(stmt.to_owned()).await?;
-        }
-
-        Ok(())
+        manager
+            .drop_table(Table::drop().table(Notes::Table).to_owned())
+            .await
     }
+}
+
+/// `Iden` is a trait for identifiers used in any query statement.
+/// 
+/// Commonly implemented by Enum where each Enum represents a table found in a database,
+/// and its variants include table name and column name.
+/// 
+/// Learn more at https://docs.rs/sea-query#iden
+#[derive(Iden)]
+enum Notes {
+    Table,
+    Id,
+    Title,
+    Text,
 }
