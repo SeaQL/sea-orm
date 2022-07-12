29132cf254107547308c74cca303b7103fe270c6
Billy Chan <ccw.billy.123@gmail.com>
2022-07-12 10:59:26 +0800

    Update deployment script

--- build-tools/cargo-publish.sh
+++ build-tools/cargo-publish.sh
@@ -30,3 +30,9 @@ sed -i 's/^sea-orm-cli [^,]*,/sea-orm-cli = { version = "\^'$1'",/' Cargo.toml
 sed -i 's/^sea-orm [^,]*,/sea-orm = { version = "\^'$1'",/' Cargo.toml
 git commit -am "sea-orm-migration $1"
 cargo publish
+cd ..
+sleep 30
+cd examples
+find . -depth -type f -name '*.toml' -exec sed -i 's/^version = "\^.*" # sea-orm version$/version = "\^'$1'" # sea-orm version/' {} \;
+find . -depth -type f -name '*.toml' -exec sed -i 's/^version = "\^.*" # sea-orm-migration version$/version = "\^'$1'" # sea-orm-migration version/' {} \;
+git commit -am "update examples"
