a77ce867022792623adaf3720b1a9e18e168aae6
Sanford Pun <punsanford@gmail.com>
2022-07-21 15:56:06 +0800

    Add mock execution

--- examples/rocket_example/core/tests/mock.rs
+++ examples/rocket_example/core/tests/mock.rs
@@ -1,7 +1,8 @@
 mod prepare;
 
+use entity::post;
 use prepare::prepare_mock_db;
-use rocket_example_core::Query;
+use rocket_example_core::{Mutation, Query};
 
 #[tokio::test]
 async fn main() {
@@ -17,5 +18,62 @@ async fn main() {
         let post = Query::find_post_by_id(db, 5).await.unwrap().unwrap();
 
         assert_eq!(post.id, 5);
+    }
+
+    {
+        let post = Mutation::create_post(
+            db,
+            post::Model {
+                id: 0,
+                title: "Title D".to_owned(),
+                text: "Text D".to_owned(),
+            },
+        )
+        .await
+        .unwrap();
+
+        assert_eq!(
+            post,
+            post::ActiveModel {
+                id: sea_orm::ActiveValue::Unchanged(6),
+                title: sea_orm::ActiveValue::Unchanged("Title D".to_owned()),
+                text: sea_orm::ActiveValue::Unchanged("Text D".to_owned())
+            }
+        );
+    }
+
+    {
+        let post = Mutation::update_post_by_id(
+            db,
+            1,
+            post::Model {
+                id: 1,
+                title: "New Title A".to_owned(),
+                text: "New Text A".to_owned(),
+            },
+        )
+        .await
+        .unwrap();
+
+        assert_eq!(
+            post,
+            post::Model {
+                id: 1,
+                title: "New Title A".to_owned(),
+                text: "New Text A".to_owned(),
+            }
+        );
+    }
+
+    {
+        let result = Mutation::delete_post(db, 5).await.unwrap();
+
+        assert_eq!(result.rows_affected, 1);
+    }
+
+    {
+        let result = Mutation::delete_all_posts(db).await.unwrap();
+
+        assert_eq!(result.rows_affected, 5);
     }
 }
--- examples/rocket_example/core/tests/prepare.rs
+++ examples/rocket_example/core/tests/prepare.rs
@@ -4,18 +4,46 @@ use sea_orm::*;
 pub fn prepare_mock_db() -> DatabaseConnection {
     MockDatabase::new(DatabaseBackend::Postgres)
         .append_query_results(vec![
-            // First query result
             vec![post::Model {
                 id: 1,
                 title: "Title A".to_owned(),
                 text: "Text A".to_owned(),
             }],
-            // Second query result
             vec![post::Model {
                 id: 5,
                 title: "Title C".to_owned(),
                 text: "Text C".to_owned(),
             }],
+            vec![post::Model {
+                id: 6,
+                title: "Title D".to_owned(),
+                text: "Text D".to_owned(),
+            }],
+            vec![post::Model {
+                id: 1,
+                title: "Title A".to_owned(),
+                text: "Text A".to_owned(),
+            }],
+            vec![post::Model {
+                id: 1,
+                title: "New Title A".to_owned(),
+                text: "New Text A".to_owned(),
+            }],
+            vec![post::Model {
+                id: 5,
+                title: "Title C".to_owned(),
+                text: "Text C".to_owned(),
+            }],
+        ])
+        .append_exec_results(vec![
+            MockExecResult {
+                last_insert_id: 6,
+                rows_affected: 1,
+            },
+            MockExecResult {
+                last_insert_id: 6,
+                rows_affected: 5,
+            },
         ])
         .into_connection()
 }
