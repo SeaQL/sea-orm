9e57574de2aaf3dc0259a6ae5d30ed82fc68aa9a
Billy Chan <ccw.billy.123@gmail.com>
2022-09-23 15:32:18 +0800

    Run migration script defined in SeaQuery (#1063)

--- sea-orm-migration/tests/migrator/mod.rs
+++ sea-orm-migration/tests/migrator/mod.rs
@@ -3,6 +3,7 @@ use sea_orm_migration::prelude::*;
 mod m20220118_000001_create_cake_table;
 mod m20220118_000002_create_fruit_table;
 mod m20220118_000003_seed_cake_table;
+mod m20220923_000001_seed_cake_table;
 
 pub struct Migrator;
 
@@ -13,6 +14,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20220118_000001_create_cake_table::Migration),
             Box::new(m20220118_000002_create_fruit_table::Migration),
             Box::new(m20220118_000003_seed_cake_table::Migration),
+            Box::new(m20220923_000001_seed_cake_table::Migration),
         ]
     }
 }
