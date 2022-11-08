be2b3e7ef58ee0f691e0aa9158782165e8b3d6c5
Billy Chan <ccw.billy.123@gmail.com>
2022-11-08 15:49:35 +0800

    Fix clippy

--- src/entity/column.rs
+++ src/entity/column.rs
@@ -355,8 +355,8 @@ impl ColumnType {
     pub(crate) fn get_enum_name(&self) -> Option<&DynIden> {
         fn enum_name(col_type: &ColumnType) -> Option<&DynIden> {
             match col_type {
-                ColumnType::Enum { name, .. } => Some(&name),
-                ColumnType::Array(col_type) => enum_name(&col_type),
+                ColumnType::Enum { name, .. } => Some(name),
+                ColumnType::Array(col_type) => enum_name(col_type),
                 _ => None,
             }
         }
