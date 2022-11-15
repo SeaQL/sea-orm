4d8645ae8b091002a84c3905aa858e4caa329da8
Billy Chan <ccw.billy.123@gmail.com>
2022-11-15 13:50:05 +0800

    Support Vector of enum for Postgres (#1210)
    * Support Vector of enum for Postgres
    
    * Fix clippy
    
    * Add tests for integer array
    
    * Fix dependency
    
    * Bump sea-query to 0.27.2
    
    Co-authored-by: Chris Tsang <chris.2y3@outlook.com>

--- tests/common/features/collection.rs
+++ tests/common/features/collection.rs
@@ -1,3 +1,4 @@
+use super::sea_orm_active_enums::*;
 use sea_orm::entity::prelude::*;
 
 #[derive(Clone, Debug, PartialEq, Eq, DeriveEntityModel)]
@@ -7,6 +8,10 @@ pub struct Model {
     pub id: i32,
     pub integers: Vec<i32>,
     pub integers_opt: Option<Vec<i32>>,
+    pub teas: Vec<Tea>,
+    pub teas_opt: Option<Vec<Tea>>,
+    pub colors: Vec<Color>,
+    pub colors_opt: Option<Vec<Color>>,
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
