2cb9824a183a374ac89695d3203b49ee6a1c1ff2
Chris Tsang <chris.2y3@outlook.com>
2023-01-10 16:44:33 +0800

    Mascot

--- src/lib.rs
+++ src/lib.rs
@@ -306,6 +306,12 @@
 //! A big shout out to our contributors:
 //!
 //! [![Contributors](https://opencollective.com/sea-orm/contributors.svg?width=1000&button=false)](https://github.com/SeaQL/sea-orm/graphs/contributors)
+//!
+//! ## Mascot
+//!
+//! A friend of Ferris, Terres the hermit crab is the official mascot of SeaORM. His hobby is collecting shells.
+//!
+//! <img alt="Terres" src="https://www.sea-ql.org/SeaORM/img/Terres.png" width="400"/>
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/SeaQL/sea-query/master/docs/SeaQL icon dark.png"
 )]
