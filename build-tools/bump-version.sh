36f1911a0203a212d429acdb835b0af45d9c8fd1
Chris Tsang <chris.2y3@outlook.com>
2023-02-04 16:10:10 +0800

    Update build script

--- build-tools/bump-version.sh
+++ build-tools/bump-version.sh
@@ -4,37 +4,31 @@ set -e
 # Bump `sea-orm-codegen` version
 cd sea-orm-codegen
 sed -i 's/^version.*$/version = "'$1'"/' Cargo.toml
-git commit -am "sea-orm-codegen $1"
 cd ..
-sleep 1
 
 # Bump `sea-orm-cli` version
 cd sea-orm-cli
 sed -i 's/^version.*$/version = "'$1'"/' Cargo.toml
 sed -i 's/^sea-orm-codegen [^,]*,/sea-orm-codegen = { version = "\='$1'",/' Cargo.toml
-git commit -am "sea-orm-cli $1"
 cd ..
-sleep 1
 
 # Bump `sea-orm-macros` version
 cd sea-orm-macros
 sed -i 's/^version.*$/version = "'$1'"/' Cargo.toml
-git commit -am "sea-orm-macros $1"
 cd ..
-sleep 1
+
+# Bump `sea-orm` version
 sed -i 's/^version.*$/version = "'$1'"/' Cargo.toml
 sed -i 's/^sea-orm-macros [^,]*,/sea-orm-macros = { version = "'$1'",/' Cargo.toml
-git commit -am "$1" # publish sea-orm
-sleep 1
 
 # Bump `sea-orm-migration` version
 cd sea-orm-migration
 sed -i 's/^version.*$/version = "'$1'"/' Cargo.toml
 sed -i 's/^sea-orm-cli [^,]*,/sea-orm-cli = { version = "'$1'",/' Cargo.toml
 sed -i 's/^sea-orm [^,]*,/sea-orm = { version = "'$1'",/' Cargo.toml
-git commit -am "sea-orm-migration $1"
 cd ..
-sleep 1
+
+git commit -am "$1"
 
 # Bump examples' dependency version
 cd examples
