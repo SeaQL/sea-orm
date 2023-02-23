80b944f93e5003f862a17339d98d1af372643b87
AmineZouitine <amine.zouitinepro@gmail.com>
2023-02-23 11:40:19 +0100

    Add exex_with_returning on the DeleteOne struct, to be able to get to get the model of a deleted element

--- src/executor/delete.rs
+++ src/executor/delete.rs
@@ -1,7 +1,9 @@
 use crate::{
-    error::*, ActiveModelTrait, ConnectionTrait, DeleteMany, DeleteOne, EntityTrait, Statement,
+    error::*, ActiveModelTrait, ColumnTrait, ConnectionTrait, DeleteMany, DeleteOne, EntityTrait,
+    IntoActiveModel, Iterable, PrimaryKeyTrait, SelectModel, SelectorRaw, Statement,
 };
-use sea_query::DeleteStatement;
+use sea_query::{DeleteStatement, Query};
+use sea_query::{Expr, FromValueTuple};
 use std::future::Future;
 
 /// Handles DELETE operations in a ActiveModel using [DeleteStatement]
@@ -28,6 +30,20 @@ where
     {
         // so that self is dropped before entering await
         exec_delete_only(self.query, db)
+    }
+
+    /// Execute a DELETE operation on one ActiveModel and return the deleted model (use `RETURNING` syntax if database supported)
+    pub async fn exec_with_returning<'b, C>(
+        self,
+        db: &'b C,
+    ) -> Result<<A::Entity as EntityTrait>::Model, DbErr>
+    where
+        <A::Entity as EntityTrait>::Model: IntoActiveModel<A>,
+        C: ConnectionTrait,
+    {
+        Deleter::new(self.query)
+            .exec_delete_and_return_deleted(self.model, db)
+            .await
     }
 }
 
@@ -59,6 +75,41 @@ impl Deleter {
         let builder = db.get_database_backend();
         exec_delete(builder.build(&self.query), db)
     }
+
+    async fn exec_delete_and_return_deleted<A, C>(
+        mut self,
+        model: A,
+        db: &C,
+    ) -> Result<<A::Entity as EntityTrait>::Model, DbErr>
+    where
+        A: ActiveModelTrait,
+        C: ConnectionTrait,
+    {
+        type Entity<A> = <A as ActiveModelTrait>::Entity;
+        type Model<A> = <Entity<A> as EntityTrait>::Model;
+        type Column<A> = <Entity<A> as EntityTrait>::Column;
+        match db.support_returning() {
+            true => {
+                let returning = Query::returning()
+                    .exprs(Column::<A>::iter().map(|c| c.select_as(Expr::col(c))));
+                self.query.returning(returning);
+                let db_backend = db.get_database_backend();
+                let found: Option<Model<A>> = SelectorRaw::<SelectModel<Model<A>>>::from_statement(
+                    db_backend.build(&self.query),
+                )
+                .one(db)
+                .await?;
+                // If we got `None` then we are updating a row that does not exist.
+                match found {
+                    Some(model) => Ok(model),
+                    None => Err(DbErr::RecordNotUpdated),
+                }
+            }
+            false => {
+                find_deleted_model_by_id(model, db).await
+            }
+        }
+    }
 }
 
 async fn exec_delete_only<C>(query: DeleteStatement, db: &C) -> Result<DeleteResult, DbErr>
@@ -76,4 +127,29 @@ where
     Ok(DeleteResult {
         rows_affected: result.rows_affected(),
     })
+}
+
+async fn find_deleted_model_by_id<A, C>(
+    model: A,
+    db: &C,
+) -> Result<<A::Entity as EntityTrait>::Model, DbErr>
+where
+    A: ActiveModelTrait,
+    C: ConnectionTrait,
+{
+    type Entity<A> = <A as ActiveModelTrait>::Entity;
+    type ValueType<A> = <<Entity<A> as EntityTrait>::PrimaryKey as PrimaryKeyTrait>::ValueType;
+
+    let primary_key_value = match model.get_primary_key_value() {
+        Some(val) => ValueType::<A>::from_value_tuple(val),
+        None => return Err(DbErr::DeleteGetPrimaryKey),
+    };
+    let found = Entity::<A>::find_by_id(primary_key_value).one(db).await?;
+    // If we cannot select the updated row from db by the cached primary key
+    match found {
+        Some(model) => Ok(model),
+        None => Err(DbErr::RecordNotFound(
+            "Failed to find deleted item".to_owned(),
+        )),
+    }
 }
