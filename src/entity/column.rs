9205a3dc99b269b6722fd840b8fb0adca675e549
Billy Chan <ccw.billy.123@gmail.com>
2023-04-03 23:20:09 +0800

    feat: implement `ColumnTypeTrait` for `ColumnDef`

--- src/entity/column.rs
+++ src/entity/column.rs
@@ -300,14 +300,25 @@ impl ColumnTypeTrait for ColumnType {
     }
 
     fn get_enum_name(&self) -> Option<&DynIden> {
-        fn enum_name(col_type: &ColumnType) -> Option<&DynIden> {
-            match col_type {
-                ColumnType::Enum { name, .. } => Some(name),
-                ColumnType::Array(col_type) => enum_name(col_type),
-                _ => None,
-            }
-        }
         enum_name(self)
+    }
+}
+
+impl ColumnTypeTrait for ColumnDef {
+    fn def(self) -> ColumnDef {
+        self
+    }
+
+    fn get_enum_name(&self) -> Option<&DynIden> {
+        enum_name(&self.col_type)
+    }
+}
+
+fn enum_name(col_type: &ColumnType) -> Option<&DynIden> {
+    match col_type {
+        ColumnType::Enum { name, .. } => Some(name),
+        ColumnType::Array(col_type) => enum_name(col_type),
+        _ => None,
     }
 }
 
