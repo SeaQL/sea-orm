b69381368d1298ea57531e7bccfb2f93ad693a4e
Yiu Tin Cheung Ivan <tciyiu@connect.ust.hk>
2023-06-15 12:05:23 +0800

    added test for ForeignKeyConstraintViolation and removed unused imports/codes

--- tests/sql_err_tests.rs
+++ tests/sql_err_tests.rs
@@ -1,17 +1,11 @@
 pub mod common;
-mod crud;
-use rust_decimal_macros::dec;
-pub use sea_orm::entity::*;
-use sea_orm::error::*;
-pub use sea_orm::{sea_query, tests_cfg, ConnectionTrait, EntityName, QueryFilter, QuerySelect};
-// use sea_query::ForeignKey;
-use uuid::Uuid;
-
 pub use common::{bakery_chain::*, setup::*, TestContext};
 use pretty_assertions::assert_eq;
-
-pub use crud::*;
-use sea_orm::DatabaseConnection;
+use rust_decimal_macros::dec;
+pub use sea_orm::{
+    entity::*, error::*, tests_cfg, DatabaseConnection, EntityName, ExecResult,
+};
+use uuid::Uuid;
 
 #[sea_orm_macros::test]
 #[cfg(any(
@@ -24,11 +18,6 @@ async fn main() {
     create_tables(&ctx.db).await.unwrap();
     test_error(&ctx.db).await;
     ctx.delete().await;
-
-    // let ctx = TestContext::new("bakery_chain_sql_err_tests_2").await;
-    // create_tables(&ctx.db2).await.unwrap();
-    // test_error_foreign(&ctx.db1, &ctx.db2).await;
-    // ctx.delete().await;
 }
 
 pub async fn test_error(db: &DatabaseConnection) {
@@ -52,10 +41,24 @@ pub async fn test_error(db: &DatabaseConnection) {
         .await
         .expect_err("inserting should fail due to duplicate primary key");
 
-    let sqlerr = error.sql_err();
-    assert_eq!(sqlerr.unwrap(), SqlErr::UniqueConstraintViolation())
+    assert_eq!(error.sql_err(), Some(SqlErr::UniqueConstraintViolation));
+
+    let fk_cake = cake::ActiveModel {
+        name: Set("fk error Cake".to_owned()),
+        price: Set(dec!(10.25)),
+        gluten_free: Set(false),
+        serial: Set(Uuid::new_v4()),
+        bakery_id: Set(Some(1000)),
+        ..Default::default()
+    };
+
+    let fk_error = fk_cake
+        .insert(db)
+        .await
+        .expect_err("create foreign key should fail with non-primary key");
+
+    assert_eq!(
+        fk_error.sql_err(),
+        Some(SqlErr::ForeignKeyConstraintViolation)
+    );
 }
-
-// pub async fn test_error_foreign(db1: &DatabaseConnection, db2: &DatabaseConnection) {
-
-// }
