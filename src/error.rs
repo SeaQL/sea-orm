ca0e5d4cb19bd6fe5ae20793f6001c20481c9c17
Billy Chan <ccw.billy.123@gmail.com>
2023-07-11 15:50:11 +0800

    error msg

--- src/error.rs
+++ src/error.rs
@@ -16,7 +16,7 @@ use thiserror::Error;
 #[derive(Error, Debug)]
 pub enum DbErr {
     /// This error can happen when the connection pool is fully-utilized
-    #[error("Failed to acquire connection from pool {0}")]
+    #[error("Failed to acquire connection from pool: {0}")]
     ConnectionAcquire(ConnAcquireErr),
     /// Runtime type conversion error
     #[error("Error converting `{from}` into `{into}`: {source}")]
