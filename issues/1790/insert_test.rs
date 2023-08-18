0a22f247d9a811c854bb97894fcdf56071262b0c
Yiu Tin Cheung Ivan <tciyiu@connect.ust.hk>
2023-08-18 15:16:05 +0800

    issue related test case #1790

---
+++ issues/1790/insert_test.rs
@@ -0,0 +1,55 @@
+mod tests {
+    #[test]
+    fn insert_do_nothing_postgres() {
+        assert_eq!(
+            Insert::<cake::ActiveModel>::new()
+                .add(cake::Model {
+                    id: 1,
+                    name: "Apple Pie".to_owned(),
+                })
+                .on_conflict(OnConflict::new()
+                    .do_nothing()
+                    .to_owned()
+                )
+                .build(DbBackend::Postgres)
+                .to_string(),
+            r#"INSERT INTO "cake" ("id", "name") VALUES (1, 'Apple Pie') ON CONFLICT DO NOTHING"#,
+        );
+    }
+
+    #[test]
+    fn insert_do_nothing_mysql() {
+        assert_eq!(
+            Insert::<cake::ActiveModel>::new()
+                .add(cake::Model {
+                    id: 1,
+                    name: "Apple Pie".to_owned(),
+                })
+                .on_conflict(OnConflict::new()
+                    .do_nothing()
+                    .to_owned()
+                )
+                .build(DbBackend::Mysql)
+                .to_string(),
+            r#"INSERT IGNORE INTO "cake" ("id", "name") VALUES (1, 'Apple Pie')"#,
+        );
+    }
+
+    #[test]
+    fn insert_do_nothing() {
+        assert_eq!(
+            Insert::<cake::ActiveModel>::new()
+                .add(cake::Model {
+                    id: 1,
+                    name: "Apple Pie".to_owned(),
+                })
+                .on_conflict(OnConflict::new()
+                    .do_nothing()
+                    .to_owned()
+                )
+                .build(DbBackend::Sqlite)
+                .to_string(),
+            r#"INSERT IGNORE INTO "cake" ("id", "name") VALUES (1, 'Apple Pie')"#,
+        );
+    }
+}
