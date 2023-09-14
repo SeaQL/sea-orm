c8168b7e794912d30ceb79d59a6d8adbf3be168b
Chris Tsang <chris.2y3@outlook.com>
2023-09-14 15:40:24 +0800

    Update sea-orm-migration/tests/common/migration/m20220118_000001_create_cake_table.rs

--- sea-orm-migration/tests/common/migration/m20220118_000001_create_cake_table.rs
+++ sea-orm-migration/tests/common/migration/m20220118_000001_create_cake_table.rs
@@ -21,6 +21,7 @@ impl MigrationTrait for Migration {
                     .to_owned(),
             )
             .await?;
+
         manager
             .create_index(
                 Index::create()
