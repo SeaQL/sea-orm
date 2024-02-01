f95bb8efc78280f5d84e7f1b433fdb978a8da9b8
Billy Chan <ccw.billy.123@gmail.com>
2024-02-01 16:25:25 +0800

    Refactor

--- sea-orm-macros/src/derives/sql_type_match.rs
+++ sea-orm-macros/src/derives/sql_type_match.rs
@@ -12,7 +12,7 @@ pub fn col_type_match(
         None => {
             let col_type = match field_type {
                 "char" => quote! { Char(None) },
-                "String" | "&str" => quote! { String(sea_orm::sea_query::StringLen::None) },
+                "String" | "&str" => quote! { string(None) },
                 "i8" => quote! { TinyInteger },
                 "u8" => quote! { TinyUnsigned },
                 "i16" => quote! { SmallInteger },
@@ -36,7 +36,7 @@ pub fn col_type_match(
                 "Json" => quote! { Json },
                 "Decimal" => quote! { Decimal(None) },
                 "Vec<u8>" => {
-                    quote! { VarBinary(sea_orm::sea_query::StringLen::None) }
+                    quote! { var_binary(None) }
                 }
                 _ => {
                     // Assumed it's ActiveEnum if none of the above type matches
