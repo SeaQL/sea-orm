cb82a07eb17e88a5b1fdba0df90d34604959a349
Anshul Sanghi <anshul@anshulsanghi.tech>
2024-03-04 14:21:57 +0530

    Revert "Fix Formatting"
    This reverts commit 16c4e66096ae4bcf9f444ef9a0462d883f7e6be0.

--- sea-orm-codegen/src/tests_cfg/duplicated_many_to_many_paths/users_saved_bills.rs
+++ sea-orm-codegen/src/tests_cfg/duplicated_many_to_many_paths/users_saved_bills.rs
@@ -16,7 +16,7 @@ pub enum Relation {
         from = "Column::BillId",
         to = "super::bills::Column::Id",
         on_update = "Cascade",
-        on_delete = "Cascade"
+        on_delete = "Cascade",
     )]
     Bills,
     #[sea_orm(
@@ -24,7 +24,7 @@ pub enum Relation {
         from = "Column::UserId",
         to = "super::users::Column::Id",
         on_update = "Cascade",
-        on_delete = "Cascade"
+        on_delete = "Cascade",
     )]
     Users,
 }
