06bdfca827599eea907888ba8c1d4b4c98950e47
ヤーノシュ <janosch.reppnow@pol.co.jp>
2024-03-29 20:01:06 +0900

    feat: enable DerivePartialModel for Option<T>

--- src/entity/partial_model.rs
+++ src/entity/partial_model.rs
@@ -5,3 +5,9 @@ pub trait PartialModelTrait: FromQueryResult {
     /// Select specific columns this [PartialModel] needs
     fn select_cols<S: SelectColumns>(select: S) -> S;
 }
+
+impl<T: PartialModelTrait> PartialModelTrait for Option<T> {
+    fn select_cols<S: SelectColumns>(select: S) -> S {
+        T::select_cols(select)
+    }
+}
--- tests/partial_model_tests.rs
+++ tests/partial_model_tests.rs
@@ -62,3 +62,9 @@ struct Nest {
     #[sea_orm(nested)]
     _foo: SimpleTest,
 }
+
+#[derive(FromQueryResult, DerivePartialModel)]
+struct NestOption {
+    #[sea_orm(nested)]
+    _foo: Option<SimpleTest>,
+}
