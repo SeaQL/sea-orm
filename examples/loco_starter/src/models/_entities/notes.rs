b63223d78d397a7c869b830a97cbcb1530af2b43
Billy Chan <ccw.billy.123@gmail.com>
2024-05-28 15:06:53 +0800

    Getting Started with Loco & SeaORM (#2230)
    * Getting Started with Loco & SeaORM
    
    * README
    
    * edit
    
    * README

---
+++ examples/loco_starter/src/models/_entities/notes.rs
@@ -0,0 +1,16 @@
+use sea_orm::entity::prelude::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, PartialEq, DeriveEntityModel, Eq, Serialize, Deserialize)]
+#[sea_orm(table_name = "notes")]
+pub struct Model {
+    pub created_at: DateTime,
+    pub updated_at: DateTime,
+    #[sea_orm(primary_key)]
+    pub id: i32,
+    pub title: Option<String>,
+    pub content: Option<String>,
+}
+
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+pub enum Relation {}
