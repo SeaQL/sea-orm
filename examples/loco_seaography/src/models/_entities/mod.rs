e4a6dca064563a66b5583d3dfe10be19bfea7cca
Billy Chan <ccw.billy.123@gmail.com>
2024-12-02 16:25:34 +0800

    fmt

--- examples/loco_seaography/src/models/_entities/mod.rs
+++ examples/loco_seaography/src/models/_entities/mod.rs
@@ -6,8 +6,4 @@ pub mod files;
 pub mod notes;
 pub mod users;
 
-seaography::register_entity_modules!([
-    files,
-    notes,
-    users,
-]);
+seaography::register_entity_modules!([files, notes, users]);
