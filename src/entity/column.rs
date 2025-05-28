753c14f2882e30de9b199ddc69486a41496751b0
Chris Tsang <chris.2y3@outlook.com>
2025-05-28 23:28:10 +0200

    Bump sea-query

--- src/entity/column.rs
+++ src/entity/column.rs
@@ -481,7 +481,7 @@ where
         }
         _ => match col_type.get_enum_name() {
             Some(enum_name) => f(expr, SeaRc::clone(enum_name), col_type),
-            None => expr.into(),
+            None => expr,
         },
     }
 }
