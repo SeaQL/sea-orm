225db4e829e2897a74b08f63543475f034368672
Chris Tsang <chris.2y3@outlook.com>
2025-06-08 21:08:52 +0100

    Small test case

--- src/tests_cfg/sea_orm_active_enums.rs
+++ src/tests_cfg/sea_orm_active_enums.rs
@@ -9,3 +9,10 @@ pub enum Tea {
     #[sea_orm(string_value = "BreakfastTea")]
     BreakfastTea,
 }
+
+#[derive(Debug, Clone, PartialEq, Eq, EnumIter, DeriveActiveEnum)]
+#[sea_orm(rs_type = "String", db_type = "String(StringLen::None)")]
+pub enum SeaORM {
+    #[sea_orm(string_value = "씨오알엠")]
+    씨오알엠,
+}
