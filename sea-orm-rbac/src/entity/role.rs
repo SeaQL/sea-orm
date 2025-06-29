1b7e315078dab99bd7486be27496f5523dc0bf59
Chris Tsang <chris.2y3@outlook.com>
2025-06-30 00:38:19 +0100

    Init entities

---
+++ sea-orm-rbac/src/entity/role.rs
@@ -0,0 +1,14 @@
+use sea_orm::entity::prelude::*;
+
+#[derive(Clone, Debug, PartialEq, Eq, DeriveEntityModel)]
+#[sea_orm(table_name = "sea_orm_role")]
+pub struct Model {
+    #[sea_orm(primary_key)]
+    pub id: i64,
+    pub name: String,
+}
+
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+pub enum Relation {}
+
+impl ActiveModelBehavior for ActiveModel {}
