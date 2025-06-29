8fde664c1dcc36570bce31af80485ab12bc39dd5
Chris Tsang <chris.2y3@outlook.com>
2025-06-30 00:38:19 +0100

    Init entities

---
+++ sea-orm-rbac/src/entity/role_permission.rs
@@ -0,0 +1,36 @@
+use sea_orm::entity::prelude::*;
+
+#[derive(Clone, Debug, PartialEq, Eq, DeriveEntityModel)]
+#[sea_orm(table_name = "sea_orm_role_permission")]
+pub struct Model {
+    #[sea_orm(primary_key)]
+    pub role_id: i64,
+    #[sea_orm(primary_key)]
+    pub permission_id: i64,
+    #[sea_orm(primary_key)]
+    pub resource_id: i64,
+}
+
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+pub enum Relation {
+    #[sea_orm(
+        belongs_to = "super::role::Entity",
+        from = "Column::RoleId",
+        to = "super::role::Column::Id"
+    )]
+    Role,
+    #[sea_orm(
+        belongs_to = "super::permission::Entity",
+        from = "Column::PermissionId",
+        to = "super::permission::Column::Id"
+    )]
+    Permission,
+    #[sea_orm(
+        belongs_to = "super::resource::Entity",
+        from = "Column::ResourceId",
+        to = "super::resource::Column::Id"
+    )]
+    Resource,
+}
+
+impl ActiveModelBehavior for ActiveModel {}
