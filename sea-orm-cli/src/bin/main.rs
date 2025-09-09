c72b69c3e5d0697725e804c4d91bc49d550288e7
狐狸 <134658521+Huliiiiii@users.noreply.github.com>
2025-09-09 16:27:57 +0800

    Allow cli use tokio without async-std (#2721)

--- sea-orm-cli/src/bin/main.rs
+++ sea-orm-cli/src/bin/main.rs
@@ -1,4 +1,21 @@
-#[async_std::main]
+#[cfg_attr(
+    any(
+        feature = "runtime-async-std",
+        feature = "runtime-async-std-native-tls",
+        feature = "runtime-async-std-rustls"
+    ),
+    async_std::main
+)]
+#[cfg_attr(
+    any(
+        feature = "runtime-tokio",
+        feature = "runtime-tokio-native-tls",
+        feature = "runtime-tokio-rustls",
+        // TODO: Remove this if the async-std is not the default runtime
+        not(feature = "runtime-async-std-native-tls")
+    ),
+    tokio::main
+)]
 async fn main() {
     sea_orm_cli::main().await
 }
