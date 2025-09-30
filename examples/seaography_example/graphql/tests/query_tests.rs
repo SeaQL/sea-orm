8313adc161d382b8401edfff056a008d61429b29
Chris Tsang <chris.2y3@outlook.com>
2025-09-30 19:06:24 +0100

    Fix CI

--- examples/seaography_example/graphql/tests/query_tests.rs
+++ examples/seaography_example/graphql/tests/query_tests.rs
@@ -4,8 +4,7 @@ use seaography::async_graphql;
 
 async fn schema() -> Schema {
     let database = Database::connect(
-        std::env::var("DATABASE_URL")
-            .unwrap_or_else(|_| "sqlite://seaography_example/bakery.db".into()),
+        std::env::var("DATABASE_URL").unwrap_or_else(|_| "sqlite://../bakery.db".into()),
     )
     .await
     .unwrap();
