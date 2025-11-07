b72c149eb89acc5f60bf2fb6fc014fa723e16483
Chris Tsang <chris.2y3@outlook.com>
2025-11-07 19:28:49 +0000

    Tweaks

--- examples/graphql_example/migration/src/m20220120_000001_create_note_table.rs
+++ examples/graphql_example/migration/src/m20220120_000001_create_note_table.rs
@@ -1,5 +1,5 @@
 use entity::note;
-use sea_orm_migration::{prelude::*, schema::*};
+use sea_orm_migration::prelude::*;
 
 #[derive(DeriveMigrationName)]
 pub struct Migration;
@@ -11,7 +11,7 @@ impl MigrationTrait for Migration {
 
         db.get_schema_builder()
             .register(note::Entity)
-            .apply(db)
+            .sync(db)
             .await
     }
 }
