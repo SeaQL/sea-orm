1922bdc1ee0f65842d58a0c22f6adab712120619
uuushiro <yushiro.ma2ta2.21@gmail.com>
2025-12-16 15:45:37 +0900

    Format connection.rs for cargo fmt

--- sea-orm-migration/src/connection.rs
+++ sea-orm-migration/src/connection.rs
@@ -1,1 +1,4 @@
-pub use sea_orm::{DatabaseExecutor as SchemaManagerConnection, IntoDatabaseExecutor as IntoSchemaManagerConnection};
+pub use sea_orm::{
+    DatabaseExecutor as SchemaManagerConnection,
+    IntoDatabaseExecutor as IntoSchemaManagerConnection,
+};
