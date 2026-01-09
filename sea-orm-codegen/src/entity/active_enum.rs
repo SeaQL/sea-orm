7cbf79124414ac59cd6862eb095058f02b814a9e
Agni <a@a4n1.com>
2026-01-09 11:55:51 +0000

    refactor control flow
    Signed-off-by: Agni <a@a4n1.com>

--- sea-orm-codegen/src/entity/active_enum.rs
+++ sea-orm-codegen/src/entity/active_enum.rs
@@ -24,22 +24,18 @@ impl ActiveEnum {
         let enum_name = &self.enum_name.to_string();
         let enum_iden = format_ident!("{}", enum_name.to_upper_camel_case());
         let values: Vec<String> = self.values.iter().map(|v| v.to_string()).collect();
-        let variants = values.iter().map(|v| v.trim()).map(|v| {
-            let variant_name = if v.is_empty() {
-                println!("Warning: item in the enumeration '{enum_name}' is an empty string, it will be converted to `__EmptyString`. You can modify it later as needed.");
-                "__EmptyString".to_string()
-            } else if v.chars().next().map(char::is_numeric).unwrap_or_default() {
-                if v.chars().any(|c| !c.is_numeric() && !c.is_alphabetic() && c != '_' && c != '-' && c != ' ') {
-                    "".to_string()
-                } else {
-                    format!("_{}", v.replace("-", "").replace(" ", ""))
-                }
-            } else {
-                v.to_upper_camel_case()
-            };
 
-            if variant_name.is_empty() {
+        let variants = values.iter().map(|v| v.trim()).map(|v| {
+            if v.is_empty() && !v.to_upper_camel_case().is_empty() {
+                println!("Warning: item in the enumeration '{enum_name}' is an empty string, it will be converted to `__EmptyString`. You can modify it later as needed.");
+                return format_ident!("__EmptyString");
+            }
+
+            if v
+                .chars()
+                .any(|c| !c.is_alphanumeric() && c != '_' && c != '-' && c != ' ') {
                 println!("Warning: item '{v}' in the enumeration '{enum_name}' cannot be converted into a valid Rust enum member name. It will be converted to its corresponding UTF-8 encoding. You can modify it later as needed.");
+
                 let mut ss = String::new();
                 for c in v.chars() {
                     if c.len_utf8() > 1 {
@@ -48,10 +44,14 @@ impl ActiveEnum {
                         write!(&mut ss, "U{:04X}", c as u32).unwrap();
                     }
                 }
-                format_ident!("{}", ss)
-            } else {
-                format_ident!("{}", variant_name)
+                return format_ident!("{}", ss);
             }
+
+            if v.chars().next().is_some_and(char::is_numeric) {
+                return format_ident!("_{}", v.replace(['-', ' '], ""));
+            }
+
+            format_ident!("{}", v.to_upper_camel_case())
         });
 
         let serde_derive = with_serde.extra_derive();
