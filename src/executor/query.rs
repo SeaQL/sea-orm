d39c87f235fda0f7436e6ddf09a7656a9c0e23c4
狐狸 <308013446a@gmail.com>
2026-03-08 08:25:47 +0800

    Fix no-default-features compile errors in #2990 and #2991 (#2992)

--- src/executor/query.rs
+++ src/executor/query.rs
@@ -804,6 +804,9 @@ try_getable_uuid!(uuid::fmt::Urn, uuid::Uuid::urn);
 #[cfg(feature = "with-ipnetwork")]
 try_getable_postgres!(ipnetwork::IpNetwork);
 
+#[cfg(feature = "with-mac_address")]
+try_getable_postgres!(mac_address::MacAddress);
+
 impl TryGetable for u32 {
     #[allow(unused_variables)]
     fn try_get_by<I: ColIdx>(res: &QueryResult, idx: I) -> Result<Self, TryGetError> {
@@ -995,6 +998,9 @@ mod postgres_array {
 
     #[cfg(feature = "with-ipnetwork")]
     try_getable_postgres_array!(ipnetwork::IpNetwork);
+
+    #[cfg(feature = "with-mac_address")]
+    try_getable_postgres_array!(mac_address::MacAddress);
 
     #[allow(unused_macros)]
     macro_rules! try_getable_postgres_array_uuid {
@@ -1536,6 +1542,9 @@ try_from_u64_err!(uuid::fmt::Braced);
 
 #[cfg(feature = "with-ipnetwork")]
 try_from_u64_err!(ipnetwork::IpNetwork);
+
+#[cfg(feature = "with-mac_address")]
+try_from_u64_err!(mac_address::MacAddress);
 
 #[cfg(test)]
 mod tests {
