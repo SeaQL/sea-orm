737938922f99dcc48aa7f14eb1964a3c06790b43
Huliiiiii <308013446a@gmail.com>
2026-03-30 22:20:47 +0800

    Refactor make-sync.sh

--- build-tools/make-sync.sh
+++ build-tools/make-sync.sh
@@ -8,68 +8,82 @@ else
     SI=(sed -i '')
 fi
 
+sed_in_place() {
+    local expression="$1"
+    shift
+    "${SI[@]}" "$expression" "$@"
+}
+
+replace_rs() {
+    local expression="$1"
+    shift
+    find "$@" -type f -name '*.rs' -exec "${SI[@]}" "$expression" {} +
+}
+
 rm -rf sea-orm-sync/src
 rm -rf sea-orm-sync/tests
 cp -r src sea-orm-sync
 cp -r tests sea-orm-sync
-cp -r examples/quickstart/src/main.rs sea-orm-sync/examples/quickstart/src/main.rs
+cp examples/quickstart/src/main.rs sea-orm-sync/examples/quickstart/src/main.rs
 rm -rf sea-orm-sync/src/bin
 cd sea-orm-sync
-find src -type f -name '*.rs' -exec "${SI[@]}" "s/Pin<Box<dyn Future<Output = Result<Self::Stream<'a>, DbErr>> + 'a + Send>>/Result<Self::Stream<'a>, DbErr>/" {} +
-find src -type f -name '*.rs' -exec "${SI[@]}" "s/Pin<Box<dyn Future<Output = Result<T, E>> + Send + 'b>>/Result<T, E>/" {} +
-find src -type f -name '*.rs' -exec "${SI[@]}" "s/Pin<Box<dyn Future<Output = Result<T, E>> + Send + 'c>>/Result<T, E>/" {} +
-find src   -type f -name '*.rs' -exec "${SI[@]}" 's/Box::pin(async move {/({/' {} +
-find tests -type f -name '*.rs' -exec "${SI[@]}" 's/Box::pin(async move {/({/' {} +
-find src   -type f -name '*.rs' -exec "${SI[@]}" 's/async //' {} +
-find tests -type f -name '*.rs' -exec "${SI[@]}" 's/async //' {} +
-find examples -type f -name '*.rs' -exec "${SI[@]}" 's/async //' {} +
-find src   -type f -name '*.rs' -exec "${SI[@]}" 's/\.await//' {} +
-find tests -type f -name '*.rs' -exec "${SI[@]}" 's/\.await//' {} +
-find examples -type f -name '*.rs' -exec "${SI[@]}" 's/\.await//' {} +
-find src   -type f -name '*.rs' -exec "${SI[@]}" '/#\[async_trait::async_trait\]/d' {} +
-find tests -type f -name '*.rs' -exec "${SI[@]}" '/#\[async_trait::async_trait\]/d' {} +
-find src -type f -name '*.rs' -exec "${SI[@]}" 's/#\[smol_potat::test\]/#\[test\]/' {} +
-find src -type f -name '*.rs' -exec "${SI[@]}" '/#\[smol_potat::main\]/d' {} +
-find examples -type f -name '*.rs' -exec "${SI[@]}" '/#\[tokio::main\]/d' {} +
-find src -type f -name '*.rs' -exec "${SI[@]}" 's/#\[tokio::test\]/#\[test\]/' {} +
-find tests -type f -name '*.rs' -exec "${SI[@]}" 's/#\[cfg(feature = "sqlx-sqlite")\]/#\[cfg(feature = "rusqlite")\]/' {} +
-find src -type f -name '*.rs' -exec "${SI[@]}" "/[a-zA-Z]+<Self>: Send + Sync,/d" {} +
-find src -type f -name '*.rs' -exec "${SI[@]}" "/[a-zA-Z]+<Self>: Send,/d" {} +
-find src -type f -name '*.rs' -exec "${SI[@]}" "s/: Send + Sync {/ {/" {} +
-find src -type f -name '*.rs' -exec "${SI[@]}" "s/type Stream<'a>: Stream/type Stream<'a>: Iterator/" {} +
-find src -type f -name '*.rs' -exec "${SI[@]}" "s/: Send {/ {/" {} +
-find src -type f -name '*.rs' -exec "${SI[@]}" "s/>: Send$/>/" {} +
-find src -type f -name '*.rs' -exec "${SI[@]}" "s/: Sync {/ {/" {} +
-find src -type f -name '*.rs' -exec "${SI[@]}" "s/Send + Sync + //" {} +
-find src -type f -name '*.rs' -exec "${SI[@]}" "s/ + Sync//" {} +
-find src -type f -name '*.rs' -exec "${SI[@]}" "s/ + Send//" {} +
-find src -type f -name '*.rs' -exec "${SI[@]}" "s/Send + //" {} +
-find src -type f -name '*.rs' -exec "${SI[@]}" 's/Arc<dyn std::error::Error>/Arc<dyn std::error::Error + Send + Sync>/' {} +
-find src -type f -name '*.rs' -exec "${SI[@]}" '/T: Send,/d' {} +
-find src -type f -name '*.rs' -exec "${SI[@]}" '/R::Model: Send,/d' {} +
-find src -type f -name '*.rs' -exec "${SI[@]}" '/S::Item: Send,/d' {} +
-find src -type f -name '*.rs' -exec "${SI[@]}" "s/Box::pin/Box::new/" {} +
-find src -type f -name '*.rs' -exec "${SI[@]}" 's/impl Stream</impl Iterator</' {} +
-find src -type f -name '*.rs' -exec "${SI[@]}" 's/S: Stream</S: Iterator</' {} +
-find src -type f -name '*.rs' -exec "${SI[@]}" 's/use futures_util::lock::Mutex;/use std::sync::Mutex;/' {} +
-find src -type f -name '*.rs' -exec "${SI[@]}" 's/use futures_util::lock::MutexGuard;/use std::sync::MutexGuard;/' {} +
-find src -type f -name '*.rs' -exec "${SI[@]}" 's/, pin::Pin//' {} +
-find src -type f -name '*.rs' -exec "${SI[@]}" 's/{pin::Pin, /{/' {} +
-find src -type f -name '*.rs' -exec "${SI[@]}" 's/{task::Poll, /{/' {} +
-find src -type f -name '*.rs' -exec "${SI[@]}" 's/{future::Future, /{/' {} +
-find src -type f -name '*.rs' -exec "${SI[@]}" 's/, task::Poll//' {} +
-find src -type f -name '*.rs' -exec "${SI[@]}" '/use std::{pin::Pin};/d' {} +
-find src -type f -name '*.rs' -exec "${SI[@]}" '/use std::{task::Poll};/d' {} +
-find src -type f -name '*.rs' -exec "${SI[@]}" '/use std::{future::Future};/d' {} +
-find src -type f -name '*.rs' -exec "${SI[@]}" 's/, future::Future//' {} +
-find src -type f -name '*.rs' -exec "${SI[@]}" '/use futures_util::Stream/d' {} +
-find src -type f -name '*.rs' -exec "${SI[@]}" '/use futures_util::{Stream/d' {} +
-find src -type f -name '*.rs' -exec "${SI[@]}" '/use futures_util::{TryStreamExt,/d' {} +
-find src -type f -name '*.rs' -exec "${SI[@]}" '/\/\/\/ use futures_util/d' {} +
-find src -type f -name '*.rs' -exec "${SI[@]}" '/use futures_util::future::BoxFuture;/d' {} +
-find src -type f -name '*.rs' -exec "${SI[@]}" '/use async_stream::/d' {} +
-find tests -type f -name '*.rs' -exec "${SI[@]}" '/use futures_util::StreamExt/d' {} +
-find src -type f -name '*.rs' -exec "${SI[@]}" 's/self.conn.try_lock()/self.conn.try_lock().ok()/' {} +
-"${SI[@]}" 's/self.conn.try_lock().ok()/self.conn.try_lock()/' ./src/driver/rusqlite.rs
-cargo fmt
+
+replace_rs "s/Pin<Box<dyn Future<Output = Result<Self::Stream<'a>, DbErr>> + 'a + Send>>/Result<Self::Stream<'a>, DbErr>/" src
+replace_rs "s/Pin<Box<dyn Future<Output = Result<T, E>> + Send + 'b>>/Result<T, E>/" src
+replace_rs "s/Pin<Box<dyn Future<Output = Result<T, E>> + Send + 'c>>/Result<T, E>/" src
+replace_rs 's/Box::pin(async move {/({/' src
+replace_rs 's/Box::pin(async move {/({/' tests
+replace_rs 's/async //' src
+replace_rs 's/async //' tests
+replace_rs 's/async //' examples
+replace_rs 's/\.await//' src
+replace_rs 's/\.await//' tests
+replace_rs 's/\.await//' examples
+replace_rs '/#\[async_trait::async_trait\]/d' src
+replace_rs '/#\[async_trait::async_trait\]/d' tests
+replace_rs 's/#\[smol_potat::test\]/#\[test\]/' src
+replace_rs '/#\[smol_potat::main\]/d' src
+replace_rs '/#\[tokio::main\]/d' examples
+replace_rs 's/#\[tokio::test\]/#\[test\]/' src
+replace_rs 's/#\[cfg(feature = "sqlx-sqlite")\]/#\[cfg(feature = "rusqlite")\]/' tests
+replace_rs '/[a-zA-Z]+<Self>: Send + Sync,/d' src
+replace_rs '/[a-zA-Z]+<Self>: Send,/d' src
+replace_rs 's/: Send + Sync {/ {/' src
+replace_rs "s/type Stream<'a>: Stream/type Stream<'a>: Iterator/" src
+replace_rs 's/: Send {/ {/' src
+replace_rs 's/>: Send$/>/' src
+replace_rs 's/: Sync {/ {/' src
+replace_rs 's/Send + Sync + //' src
+replace_rs 's/ + Sync//' src
+replace_rs 's/ + Send//' src
+replace_rs 's/Send + //' src
+replace_rs 's/Arc<dyn std::error::Error>/Arc<dyn std::error::Error + Send + Sync>/' src
+replace_rs '/T: Send,/d' src
+replace_rs '/R::Model: Send,/d' src
+replace_rs '/S::Item: Send,/d' src
+replace_rs 's/Box::pin/Box::new/' src
+replace_rs 's/impl Stream</impl Iterator</' src
+replace_rs 's/S: Stream</S: Iterator</' src
+replace_rs 's/use futures_util::lock::Mutex;/use std::sync::Mutex;/' src
+replace_rs 's/use futures_util::lock::MutexGuard;/use std::sync::MutexGuard;/' src
+replace_rs 's/, pin::Pin//' src
+replace_rs 's/{pin::Pin, /{/' src
+replace_rs 's/{task::Poll, /{/' src
+replace_rs 's/{future::Future, /{/' src
+replace_rs 's/, task::Poll//' src
+replace_rs '/use std::{pin::Pin};/d' src
+replace_rs '/use std::{task::Poll};/d' src
+replace_rs '/use std::{future::Future};/d' src
+replace_rs 's/, future::Future//' src
+replace_rs '/use futures_util::Stream/d' src
+replace_rs '/use futures_util::{Stream/d' src
+replace_rs '/use futures_util::{TryStreamExt,/d' src
+replace_rs '/\/\/\/ use futures_util/d' src
+replace_rs '/use futures_util::future::BoxFuture;/d' src
+replace_rs '/use async_stream::/d' src
+replace_rs '/use futures_util::StreamExt/d' tests
+replace_rs 's/self.conn.try_lock()/self.conn.try_lock().ok()/' src
+
+sed_in_place 's/self.conn.try_lock().ok()/self.conn.try_lock()/' ./src/driver/rusqlite.rs
+
 cargo +nightly fmt
