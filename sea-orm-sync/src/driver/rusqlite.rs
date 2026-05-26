dd073b44bc9d5de0ada4e27108058ac72802eabf
狐狸 <huliiiiii.nya@gmail.com>
2026-05-26 18:39:35 +0800

    Use wasm-compatible `Instant` for wasm32 target (#3071)

--- sea-orm-sync/src/driver/rusqlite.rs
+++ sea-orm-sync/src/driver/rusqlite.rs
@@ -1,9 +1,13 @@
+#[cfg(not(target_arch = "wasm32"))]
+use std::time::Instant;
 use std::{
     ops::Deref,
     sync::{Arc, Mutex, MutexGuard, TryLockError},
-    time::{Duration, Instant},
+    time::Duration,
 };
 use tracing::{debug, instrument, warn};
+#[cfg(target_arch = "wasm32")]
+use web_time::Instant;
 
 pub use OwnedRow as RusqliteRow;
 use rusqlite::{
