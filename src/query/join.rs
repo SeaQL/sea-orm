b276dbdd28ed636f60cb39fc1512b62fea0d8800
Leo Camus <leo.camus23@gmail.com>
2026-09-14 11:19:53 +0200

    Fix left_join_linked joining multi-hop links onto the wrong alias (#3199)
    Since `left_join_linked` can be called several times, table aliases come from
    `linked_index`, but each non-first hop still joined onto `r{i-1}`, the alias
    from the first linked call. Chaining a multi-hop link after another link
    produced a join onto the previous link's tables.
    
    ---------
    
    Co-authored-by: Huliiiiii <huliiiiii.nya@gmail.com>

--- src/query/join.rs
+++ src/query/join.rs
@@ -110,7 +110,7 @@ where
             self.linked_index += 1;
             let to_tbl = format!("r{r}").into_iden();
             let from_tbl = if i > 0 {
-                format!("r{}", i - 1).into_iden()
+                format!("r{}", r - 1).into_iden()
             } else {
                 rel.from_tbl.sea_orm_table().clone()
             };
@@ -813,6 +813,27 @@ mod tests {
                 "INNER JOIN `cake_filling` AS `r2` ON `r2`.`filling_id` = `r1`.`id`",
                 "INNER JOIN `cte` AS `r3` ON `r3`.`id` = `r2`.`cake_id`))",
                 "SELECT `cake`.`id`, `cake`.`name` FROM `cte` AS `cake`",
+            ]
+            .join(" ")
+        );
+    }
+
+    #[test]
+    fn chained_left_join_linked_uses_previous_hop_alias() {
+        assert_eq!(
+            cake::Entity::find()
+                .left_join_linked(entity_linked::CakeToFilling)
+                .left_join_linked(entity_linked::CakeToFilling)
+                .select_only()
+                .column(cake::Column::Id)
+                .build(DbBackend::MySql)
+                .to_string(),
+            [
+                r"SELECT `cake`.`id` FROM `cake`",
+                r"LEFT JOIN `cake_filling` AS `r0` ON `cake`.`id` = `r0`.`cake_id`",
+                r"LEFT JOIN `filling` AS `r1` ON `r0`.`filling_id` = `r1`.`id`",
+                r"LEFT JOIN `cake_filling` AS `r2` ON `cake`.`id` = `r2`.`cake_id`",
+                r"LEFT JOIN `filling` AS `r3` ON `r2`.`filling_id` = `r3`.`id`",
             ]
             .join(" ")
         );
